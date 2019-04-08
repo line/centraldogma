line/centraldogma

commit 7b7ea60cdb5def582b2218fd53eba2fb038b4aff

Cache the comparison result between two Git trees / Fail the timed-ou… (#375)
Related commits:

- 47764e9f098f4abc2de811962bdc3294e8540c50
- 422cec92ab1b4999e52277097883bacd4c15292e
- 674d4ab03c0aef95448d64e565acd9b4e21339e5

Motivation:

- Downgrading jGit from 5.3.0 to 5.2.1 did not help.
- We should reuse the `RepositoryCache` for caching the comparison result
  between two Git trees.
- We should fail more timed-out requests fast.

Modifications:

- Updated jGit from 5.2.1 to 5.3.0.
- Moved `server.internal.storage.cache.RepositoryCache` and `CacheableCall`
  up to `server.internal.storage`.
- `GitRepository` now depends on `RepositoryCache` optionally.
- Added `CacheableCompareTreesCall` and took advantage of the cache in
  `GitRepository.findLatestRevision()`.
- Removed the forked `DiffScanner` and `DiffEntry`.
- Added `FailFastUtil` and used it in most `GitRepository` operations.
- Modified `GitRepository.commit0()` to return the `List<DiffEntry>`
  produced during validation, so that `notifyWatchers()` does not need
  to compare the trees but reuse what's returned by `commit0()`.
- Used `StampedLock` instead of `ReadWriteLock` to make `watch()`
  completely async.
  - Added `ReadLockStamp` to implement reentrancy.
- Miscellaneous:
  - Added more test cases to `WatchTest`.
  - Added `RepositoryCache.get()` and `getIfPresent()` to reduce unsafe
    casts on the caller side.
  - Removed unnecessary `try { ... } catch (Exception) { ... }` blocks.
  - Renamed `GitRepository.toTreeId()` to `toTree()` and changed it to
    return `RevTree` instead of `ObjectId`.
  - De-duplicated anonymous `PathEdit` implementations into inner classes.

Result:

- The comparison result between two Git trees are now stored in
  `RepositoryCache` which can be configured by a user.
- All `GitRepository` operations fail fast when timed out.

server/src/main/java/com/linecorp/centraldogma/server/internal/storage/repository/git/CommitWatchers.java
@@ -70,6 +70,10 @@ final class CommitWatchers {
     void notify(Revision revision, String path) {
         List<Watch> eligibleWatches = null;
         synchronized (watchesMap) {
+            if (watchesMap.isEmpty()) {
+                return;
+            }
+
             for (final Entry<PathPatternFilter, Set<Watch>> e : watchesMap.entrySet()) {
                 if (!e.getKey().matches(path)) {
                     continue;

common/src/main/java/com/linecorp/centraldogma/common/Revision.java
@@ -21,6 +21,8 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.regex.Pattern;
 
+import javax.annotation.Nullable;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
@@ -206,7 +208,7 @@ public class Revision implements Comparable<Revision> {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(@Nullable Object o) {
         if (this == o) {
             return true;
         }

dependencies.yml
@@ -189,7 +189,7 @@ org.eclipse.jetty.alpn:
   alpn-api: { version: '1.1.3.v20160715' }
 
 org.eclipse.jgit:
-  org.eclipse.jgit: { version: '5.2.1.201812262042-r' }
+  org.eclipse.jgit: { version: '5.3.0.201903130848-r' }
 
 org.hibernate.validator:
   hibernate-validator: { version: '6.0.15.Final' }

it/src/test/java/com/linecorp/centraldogma/it/WatchTest.java
@@ -63,9 +63,7 @@ public class WatchTest extends AbstractMultiClientTest {
 
     @Test
     public void testWatchRepository() throws Exception {
-        final Revision rev1 = client()
-                                  .normalizeRevision(rule.project(), rule.repo1(), Revision.HEAD)
-                                  .join();
+        final Revision rev1 = client().normalizeRevision(rule.project(), rule.repo1(), Revision.HEAD).join();
 
         final CompletableFuture<Revision> future =
                 client().watchRepository(rule.project(), rule.repo1(), rev1, "/**", 3000);
@@ -82,6 +80,55 @@ public class WatchTest extends AbstractMultiClientTest {
         final Revision rev2 = result.revision();
 
         assertThat(rev2).isEqualTo(rev1.forward(1));
+        assertThat(future.get(3, TimeUnit.SECONDS)).isEqualTo(rev2);
+    }
+
+    @Test
+    public void testWatchRepositoryImmediateWakeup() throws Exception {
+        final Revision rev1 = client().normalizeRevision(rule.project(), rule.repo1(), Revision.HEAD).join();
+        final Change<JsonNode> change = Change.ofJsonUpsert("/test/test3.json",
+                                                            "[" + System.currentTimeMillis() + ", " +
+                                                            System.nanoTime() + ']');
+
+        final PushResult result = client().push(
+                rule.project(), rule.repo1(), rev1, "Add test3.json", change).join();
+
+        final Revision rev2 = result.revision();
+
+        assertThat(rev2).isEqualTo(rev1.forward(1));
+
+        final CompletableFuture<Revision> future =
+                client().watchRepository(rule.project(), rule.repo1(), rev1, "/**", 3000);
+        assertThat(future.get(3, TimeUnit.SECONDS)).isEqualTo(rev2);
+    }
+
+    @Test
+    public void testWatchRepositoryWithUnrelatedChange() throws Exception {
+        final Revision rev0 = client().normalizeRevision(rule.project(), rule.repo1(), Revision.HEAD).join();
+        final CompletableFuture<Revision> future =
+                client().watchRepository(rule.project(), rule.repo1(), rev0, "/test/test4.json", 3000);
+
+        final Change<JsonNode> change1 = Change.ofJsonUpsert("/test/test3.json",
+                                                             "[" + System.currentTimeMillis() + ", " +
+                                                             System.nanoTime() + ']');
+        final Change<JsonNode> change2 = Change.ofJsonUpsert("/test/test4.json",
+                                                             "[" + System.currentTimeMillis() + ", " +
+                                                             System.nanoTime() + ']');
+
+        final PushResult result1 = client().push(
+                rule.project(), rule.repo1(), rev0, "Add test3.json", change1).join();
+        final Revision rev1 = result1.revision();
+        assertThat(rev1).isEqualTo(rev0.forward(1));
+
+        // Ensure that the watcher is not notified because the path pattern does not match test3.json.
+        assertThatThrownBy(() -> future.get(500, TimeUnit.MILLISECONDS)).isInstanceOf(TimeoutException.class);
+
+        final PushResult result2 = client().push(
+                rule.project(), rule.repo1(), rev1, "Add test4.json", change2).join();
+        final Revision rev2 = result2.revision();
+        assertThat(rev2).isEqualTo(rev1.forward(1));
+
+        // Now it should be notified.
         assertThat(future.get(3, TimeUnit.SECONDS)).isEqualTo(rev2);
     }
 

server/src/jmh/java/com/linecorp/centraldogma/server/internal/storage/repository/git/GitRepositoryBenchmark.java
@@ -57,7 +57,7 @@ public class GitRepositoryBenchmark {
     public void init() throws Exception {
         repoDir = Files.createTempDirectory("jmh-gitrepository.").toFile();
         repo = new GitRepository(mock(Project.class), repoDir, format, ForkJoinPool.commonPool(),
-                                 System.currentTimeMillis(), AUTHOR);
+                                 System.currentTimeMillis(), AUTHOR, null);
         currentRevision = 1;
 
         for (int i = 0; i < previousCommits; i++) {

server/src/main/java/com/linecorp/centraldogma/server/CentralDogmaBuilder.java
@@ -19,7 +19,7 @@ package com.linecorp.centraldogma.server;
 import static com.linecorp.centraldogma.server.auth.AuthConfig.DEFAULT_SESSION_CACHE_SPEC;
 import static com.linecorp.centraldogma.server.auth.AuthConfig.DEFAULT_SESSION_TIMEOUT_MILLIS;
 import static com.linecorp.centraldogma.server.auth.AuthConfig.DEFAULT_SESSION_VALIDATION_SCHEDULE;
-import static com.linecorp.centraldogma.server.internal.storage.repository.cache.RepositoryCache.validateCacheSpec;
+import static com.linecorp.centraldogma.server.internal.storage.repository.RepositoryCache.validateCacheSpec;
 import static java.util.Objects.requireNonNull;
 
 import java.io.File;

server/src/main/java/com/linecorp/centraldogma/server/CentralDogmaConfig.java
@@ -23,7 +23,7 @@ import static com.linecorp.centraldogma.server.CentralDogmaBuilder.DEFAULT_MAX_N
 import static com.linecorp.centraldogma.server.CentralDogmaBuilder.DEFAULT_NUM_MIRRORING_THREADS;
 import static com.linecorp.centraldogma.server.CentralDogmaBuilder.DEFAULT_NUM_REPOSITORY_WORKERS;
 import static com.linecorp.centraldogma.server.CentralDogmaBuilder.DEFAULT_REPOSITORY_CACHE_SPEC;
-import static com.linecorp.centraldogma.server.internal.storage.repository.cache.RepositoryCache.validateCacheSpec;
+import static com.linecorp.centraldogma.server.internal.storage.repository.RepositoryCache.validateCacheSpec;
 import static java.util.Objects.requireNonNull;
 
 import java.io.File;

commit e2f42523c04c8febbe1fa891a111ee68ce42f8a9

Fix a watcher leak
Motivation:

When the caller of `GitRepository.watchRepository()` cancels the
returned future, the cancellation must be propagated to the future added
to `CommitWatchers`. However, it does not. This regression has been
introduced by: 7b7ea60cdb5def582b2218fd53eba2fb038b4aff

Modifications:

- Propagated the result of the returned future to the future added to
  `CommitWatchers`.
- Updated `GitRepositoryTest` to make sure that all watches are cleaned
  up.
- Fixed a bug where `CommitWatchers.watcherMap` can grow as much as the
  number of path patterns.

Result:

- No more memory leak.

server/src/main/java/com/linecorp/centraldogma/server/internal/storage/repository/git/CommitWatchers.java
@@ -33,6 +33,8 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import com.linecorp.centraldogma.common.CentralDogmaException;
 import com.linecorp.centraldogma.common.Revision;
 
@@ -40,7 +42,8 @@ final class CommitWatchers {
 
     private static final Logger logger = LoggerFactory.getLogger(CommitWatchers.class);
 
-    private final Map<PathPatternFilter, Set<Watch>> watchesMap = new WatcherMap(8192);
+    @VisibleForTesting
+    final Map<PathPatternFilter, Set<Watch>> watchesMap = new WatcherMap(8192);
 
     void add(Revision lastKnownRev, String pathPattern, CompletableFuture<Revision> future) {
         add0(PathPatternFilter.of(pathPattern), new Watch(lastKnownRev, future));
@@ -62,7 +65,11 @@ final class CommitWatchers {
             // Remove manually only when the watch was not removed from the set successfully.
             // This usually happens when a user cancels the promise.
             synchronized (watchesMap) {
-                watchesMap.get(pathPattern).remove(watch);
+                final Set<Watch> watches = watchesMap.get(pathPattern);
+                watches.remove(watch);
+                if (watches.isEmpty()) {
+                    watchesMap.remove(pathPattern);
+                }
             }
         });
     }
@@ -74,12 +81,15 @@ final class CommitWatchers {
                 return;
             }
 
-            for (final Entry<PathPatternFilter, Set<Watch>> e : watchesMap.entrySet()) {
-                if (!e.getKey().matches(path)) {
+            for (final Iterator<Entry<PathPatternFilter, Set<Watch>>> mapIt = watchesMap.entrySet().iterator();
+                 mapIt.hasNext();) {
+
+                final Entry<PathPatternFilter, Set<Watch>> entry = mapIt.next();
+                if (!entry.getKey().matches(path)) {
                     continue;
                 }
 
-                final Set<Watch> watches = e.getValue();
+                final Set<Watch> watches = entry.getValue();
                 for (final Iterator<Watch> i = watches.iterator(); i.hasNext();) {
                     final Watch w = i.next();
                     final Revision lastKnownRevision = w.lastKnownRevision;
@@ -88,6 +98,10 @@ final class CommitWatchers {
                     } else {
                         logIneligibleFuture(lastKnownRevision, revision);
                     }
+                }
+
+                if (watches.isEmpty()) {
+                    mapIt.remove();
                 }
             }
         }

server/src/main/java/com/linecorp/centraldogma/server/internal/storage/repository/git/GitRepository.java
@@ -144,7 +144,8 @@ class GitRepository implements Repository {
     private final org.eclipse.jgit.lib.Repository jGitRepository;
     private final GitRepositoryFormat format;
     private final CommitIdDatabase commitIdDatabase;
-    private final CommitWatchers commitWatchers = new CommitWatchers();
+    @VisibleForTesting
+    final CommitWatchers commitWatchers = new CommitWatchers();
     private final AtomicReference<Supplier<CentralDogmaException>> closePending = new AtomicReference<>();
     private final CompletableFuture<Void> closeFuture = new CompletableFuture<>();
 
@@ -1346,6 +1347,7 @@ class GitRepository implements Repository {
         final ServiceRequestContext ctx = context();
         final Revision normLastKnownRevision = normalizeNow(lastKnownRevision);
         final AtomicReference<ReadLockStamp> readLockStamp = new AtomicReference<>();
+        final AtomicReference<CompletableFuture<Revision>> watchFuture = new AtomicReference<>();
         final CompletableFuture<Revision> f = CompletableFuture.runAsync(() -> {
             readLockStamp.set(readLock(null));
         }, repositoryWorker).thenCompose(unused -> {
@@ -1358,17 +1360,31 @@ class GitRepository implements Repository {
             try {
                 final CompletableFuture<Revision> future = new CompletableFuture<>();
                 commitWatchers.add(normLastKnownRevision, pathPattern, future);
+                watchFuture.set(future);
                 return future;
             } finally {
                 readLockStamp.getAndSet(null).unlock();
             }
         });
 
-        f.handle((unused1, unused2) -> {
+        f.handle((result, cause) -> {
             final ReadLockStamp stamp = readLockStamp.get();
             if (stamp != null) {
                 stamp.unlock();
             }
+
+            final CompletableFuture<Revision> future = watchFuture.get();
+            if (future != null) {
+                if (cause == null) {
+                    // Should never reach here unless a user set the result explicitly,
+                    // but we handle this case anyway just in case.
+                    future.complete(result);
+                } else {
+                    // Reaches here when a user failed or cancelled the watch.
+                    future.completeExceptionally(cause);
+                }
+            }
+
             return null;
         });
 

server/src/test/java/com/linecorp/centraldogma/server/internal/storage/repository/git/GitRepositoryTest.java
@@ -22,6 +22,7 @@ import static java.util.concurrent.ForkJoinPool.commonPool;
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -1060,6 +1061,8 @@ public class GitRepositoryTest {
         assertThat(f.get(3, TimeUnit.SECONDS)).isEqualTo(rev2);
 
         assertThat(repo.normalizeNow(HEAD)).isEqualTo(rev2);
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1080,6 +1083,8 @@ public class GitRepositoryTest {
         repo.commit(rev2, 0L, Author.UNKNOWN, SUMMARY, jsonUpserts[1]).join();
         assertThat(repo.normalizeNow(HEAD)).isEqualTo(rev3);
         assertThat(f.get(3, TimeUnit.SECONDS)).isEqualTo(rev3);
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1092,6 +1097,8 @@ public class GitRepositoryTest {
         // Should notify very soon.
         final CompletableFuture<Revision> f = repo.watch(lastKnownRev, Repository.ALL_PATH);
         assertThat(f.get(3, TimeUnit.SECONDS)).isEqualTo(latestRev);
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1111,6 +1118,8 @@ public class GitRepositoryTest {
                                                   jsonUpserts[1]).join();
         assertThat(repo.normalizeNow(HEAD)).isEqualTo(newLatestRev);
         assertThat(f.get(3, TimeUnit.SECONDS)).isEqualTo(newLatestRev);
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1143,6 +1152,8 @@ public class GitRepositoryTest {
         assertThat(res.revision()).isEqualTo(rev3);
         assertThat(res.type()).isEqualTo(EntryType.JSON);
         assertThat(res.content()).isEqualTo(TextNode.valueOf("jupiter"));
+
+        ensureWatcherCleanUp();
     }
 
     @Test(timeout = 10000)
@@ -1158,6 +1169,8 @@ public class GitRepositoryTest {
         assertThat(res.type()).isEqualTo(EntryType.TEXT);
         // Text must be sanitized so that the last line ends with \n.
         assertThat(res.content()).isEqualTo(textUpserts[1].content() + '\n');
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1195,6 +1208,8 @@ public class GitRepositoryTest {
         assertThat(res.revision()).isEqualTo(rev2);
         assertThat(res.type()).isEqualTo(EntryType.JSON);
         assertThatJson(res.content()).isEqualTo(upsert2.content());
+
+        ensureWatcherCleanUp();
     }
 
     @Test
@@ -1232,6 +1247,13 @@ public class GitRepositoryTest {
 
         // No new subtask should be spawned.
         assertThat(numSubtasks.get()).isEqualTo(1);
+
+        ensureWatcherCleanUp();
+    }
+
+    private static void ensureWatcherCleanUp() {
+        // Make sure CommitWatchers has cleared the watch.
+        await().untilAsserted(() -> assertThat(repo.commitWatchers.watchesMap).isEmpty());
     }
 
     @Test