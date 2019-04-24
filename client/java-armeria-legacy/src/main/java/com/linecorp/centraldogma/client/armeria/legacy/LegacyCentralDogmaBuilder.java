line/centraldogma

commit 20583b1e74d583a5d5d12412e9cdb8af232f460f

HTTP-based Java client (#350)
Motivation:

Thrift support has been deprecated long time ago. We have to provide an
HTTP-based version of Java client for Central Dogma.

Modifications:

- Added `ArmeriaCentralDogma` and its builder.
- Added `AbstractCentralDogma` to share the common code between client
  implementations.
- (Breaking) Replaced `RepositoryInfo.lastCommit` with `headRevision`.
- Added `AuthorizationException` so that a user knows when
  authentication failure or permission error occurred.
- Added `AbstractMultiClientTest` to run the integration tests with the
  new client as well as the legacy one.
  - Needed to update the test code to make it work even if the tests run
    more than once.
- Added a new integration test: `MergeTest`
- Added `CentralDogmaRule.legacyClient()`
  - `CentralDogmaRule.client()` now uses the new client.
- Added `CentralDogmaRule.configureClient(ArmeriaCentralDogmaBuilder)`.
- Updated the documentation to use the new client.
- Various clean up in `LegacyCentralDogma` and its builder:
  - Deprecated `LegacyCentralDogmaBuilder`.
  - Added various client-side parameter validations to
    `LegacyCentralDogma` so that the server does not waste time with bad
    input.
  - Fixed incorrect application order of
    `LegacyCentralDogmaBuilder.clientConfigurator()`, which allowed a
    bad user can specify a different `ClientFactory` than what's
    actually configured.
  - Renamed `CentralDogmaClientTimeoutScheduler` to
    `LegacyCentralDogmaTimeoutScheduler`.
- Miscellaneous:
  - Implemented preview-diff operation for HTTP API v1.
  - Fixed `IllegalStateException` which is raised by `Entry.toString()`
    when an `Entry` has no content.
  - Fixed incorrect conversion from `Entry` to `EntryDto` which occurs
    when an `Entry` content is an empty string or a null node.

Result:

- A user can use the new client based on the HTTP API v1.
- HTTP API v1 now has preview-diff operation.

client/java-armeria-legacy/src/main/java/com/linecorp/centraldogma/client/armeria/legacy/LegacyCentralDogmaBuilder.java
@@ -19,12 +19,20 @@ import java.net.UnknownHostException;
 
 import com.linecorp.armeria.client.ClientBuilder;
 import com.linecorp.armeria.client.Endpoint;
+import com.linecorp.armeria.client.encoding.HttpDecodingClient;
 import com.linecorp.armeria.common.HttpHeaderNames;
 import com.linecorp.centraldogma.client.CentralDogma;
 import com.linecorp.centraldogma.client.armeria.AbstractArmeriaCentralDogmaBuilder;
+import com.linecorp.centraldogma.client.armeria.ArmeriaCentralDogmaBuilder;
 import com.linecorp.centraldogma.internal.CsrfToken;
 import com.linecorp.centraldogma.internal.thrift.CentralDogmaService;
 
+/**
+ * Builds a legacy {@link CentralDogma} client based on Thrift.
+ *
+ * @deprecated Use {@link ArmeriaCentralDogmaBuilder}.
+ */
+@Deprecated
 public class LegacyCentralDogmaBuilder extends AbstractArmeriaCentralDogmaBuilder<LegacyCentralDogmaBuilder> {
     /**
      * Returns a newly-created {@link CentralDogma} instance.
@@ -35,18 +43,20 @@ public class LegacyCentralDogmaBuilder extends AbstractArmeriaCentralDogmaBuilde
         final Endpoint endpoint = endpoint();
         final String scheme = "tbinary+" + (isUseTls() ? "https" : "http") + "://";
         final String uri = scheme + endpoint.authority() + "/cd/thrift/v1";
-        final ClientBuilder builder = new ClientBuilder(uri)
-                .factory(clientFactory())
-                .rpcDecorator(CentralDogmaClientTimeoutScheduler::new);
+        final ClientBuilder builder = new ClientBuilder(uri);
         clientConfigurator().configure(builder);
+        builder.factory(clientFactory())
+               .decorator(HttpDecodingClient.newDecorator())
+               .rpcDecorator(LegacyCentralDogmaTimeoutScheduler::new);
 
         builder.decorator((delegate, ctx, req) -> {
             if (!req.headers().contains(HttpHeaderNames.AUTHORIZATION)) {
                 // To prevent CSRF attack, we add 'Authorization' header to every request.
-                req.headers().set(HttpHeaderNames.AUTHORIZATION, "bearer " + CsrfToken.ANONYMOUS);
+                req.headers().set(HttpHeaderNames.AUTHORIZATION, "Bearer " + CsrfToken.ANONYMOUS);
             }
             return delegate.execute(ctx, req);
         });
-        return new LegacyCentralDogma(clientFactory(), builder.build(CentralDogmaService.AsyncIface.class));
+        return new LegacyCentralDogma(clientFactory().eventLoopGroup(),
+                                      builder.build(CentralDogmaService.AsyncIface.class));
     }
 }

client/java-armeria/src/main/java/com/linecorp/centraldogma/client/armeria/ArmeriaCentralDogmaBuilder.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.centraldogma.client.armeria;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import java.net.UnknownHostException;
+
+import com.linecorp.armeria.client.ClientBuilder;
+import com.linecorp.armeria.client.Endpoint;
+import com.linecorp.armeria.client.HttpClient;
+import com.linecorp.armeria.client.encoding.HttpDecodingClient;
+import com.linecorp.centraldogma.client.CentralDogma;
+import com.linecorp.centraldogma.internal.CsrfToken;
+
+public final class ArmeriaCentralDogmaBuilder
+        extends AbstractArmeriaCentralDogmaBuilder<ArmeriaCentralDogmaBuilder> {
+
+    private String accessToken = CsrfToken.ANONYMOUS;
+
+    /**
+     * Sets the access token to use when authenticating a client.
+     */
+    public ArmeriaCentralDogmaBuilder accessToken(String accessToken) {
+        requireNonNull(accessToken, "accessToken");
+        checkArgument(!accessToken.isEmpty(), "accessToken is empty.");
+        this.accessToken = accessToken;
+        return this;
+    }
+
+    /**
+     * Returns a newly-created {@link CentralDogma} instance.
+     *
+     * @throws UnknownHostException if failed to resolve the host names from the DNS servers
+     */
+    public CentralDogma build() throws UnknownHostException {
+        final Endpoint endpoint = endpoint();
+        final String scheme = "none+" + (isUseTls() ? "https" : "http") + "://";
+        final String uri = scheme + endpoint.authority();
+        final ClientBuilder builder = new ClientBuilder(uri);
+        clientConfigurator().configure(builder);
+        builder.factory(clientFactory());
+        builder.decorator(HttpDecodingClient.newDecorator());
+        return new ArmeriaCentralDogma(clientFactory().eventLoopGroup(),
+                                       builder.build(HttpClient.class), accessToken);
+    }
+}

client/java-armeria-legacy/src/main/java/com/linecorp/centraldogma/client/armeria/legacy/LegacyCentralDogmaTimeoutScheduler.java
@@ -28,12 +28,12 @@ import com.linecorp.centraldogma.internal.api.v1.WatchTimeout;
 /**
  * Decorates a {@link Client} to enlarge responseTimeout when requesting watchFile or watchRepository.
  */
-class CentralDogmaClientTimeoutScheduler extends SimpleDecoratingClient<RpcRequest, RpcResponse> {
+class LegacyCentralDogmaTimeoutScheduler extends SimpleDecoratingClient<RpcRequest, RpcResponse> {
 
     /**
      * Creates a new instance that decorates the specified {@link Client}.
      */
-    CentralDogmaClientTimeoutScheduler(Client<RpcRequest, RpcResponse> delegate) {
+    LegacyCentralDogmaTimeoutScheduler(Client<RpcRequest, RpcResponse> delegate) {
         super(delegate);
     }
 

client/java-armeria-legacy/src/test/java/com/linecorp/centraldogma/client/armeria/legacy/LegacyCentralDogmaTest.java
@@ -39,7 +39,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
-import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.common.CommonPools;
 import com.linecorp.centraldogma.client.CentralDogma;
 import com.linecorp.centraldogma.client.RepositoryInfo;
 import com.linecorp.centraldogma.client.armeria.legacy.ThriftTypes.TAuthor;
@@ -85,7 +85,7 @@ public class LegacyCentralDogmaTest {
 
     @Before
     public void setup() {
-        client = new LegacyCentralDogma(ClientFactory.DEFAULT, iface);
+        client = new LegacyCentralDogma(CommonPools.workerGroup(), iface);
     }
 
     @Test
@@ -189,13 +189,7 @@ public class LegacyCentralDogmaTest {
             return null;
         }).when(iface).listRepositories(any(), any());
         assertThat(client.listRepositories("project").get()).isEqualTo(ImmutableMap.of(
-                "repo",
-                new RepositoryInfo(
-                        "repo",
-                        new Commit(new Revision(42),
-                                   new Author("hitchhiker", "arthur@dent.com"),
-                                   Instant.parse("1978-03-08T00:00:00Z").toEpochMilli(),
-                                   "The primary phrase", "", Markup.PLAINTEXT))));
+                "repo", new RepositoryInfo("repo", new Revision(42))));
         verify(iface).listRepositories(eq("project"), any());
     }
 

client/java-armeria-legacy/src/test/java/com/linecorp/centraldogma/client/armeria/legacy/LegacyCentralDogmaTimeoutSchedulerTest.java
@@ -47,18 +47,18 @@ import com.linecorp.centraldogma.internal.thrift.CentralDogmaService;
 
 import io.netty.channel.DefaultEventLoop;
 
-public class CentralDogmaTimeoutSchedulerTest {
+public class LegacyCentralDogmaTimeoutSchedulerTest {
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
 
     @Mock
     private Client<RpcRequest, RpcResponse> client;
 
-    private CentralDogmaClientTimeoutScheduler decorator;
+    private LegacyCentralDogmaTimeoutScheduler decorator;
 
     @Before
     public void setup() {
-        decorator = new CentralDogmaClientTimeoutScheduler(client);
+        decorator = new LegacyCentralDogmaTimeoutScheduler(client);
     }
 
     @Test

client/java-armeria/src/test/java/com/linecorp/centraldogma/client/armeria/ArmeriaCentralDogmaTest.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.centraldogma.client.armeria;
+
+import static com.linecorp.centraldogma.client.armeria.ArmeriaCentralDogma.encodePathPattern;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class ArmeriaCentralDogmaTest {
+    @Test
+    public void testEncodePathPattern() {
+        assertThat(encodePathPattern("/")).isEqualTo("/");
+        assertThat(encodePathPattern(" ")).isEqualTo("%20");
+        assertThat(encodePathPattern("  ")).isEqualTo("%20%20");
+        assertThat(encodePathPattern("a b")).isEqualTo("a%20b");
+        assertThat(encodePathPattern(" a ")).isEqualTo("%20a%20");
+
+        // No new string has to be created when escaping is not necessary.
+        final String pathPatternThatDoesNotNeedEscaping = "/*.zip,/**/*.jar";
+        assertThat(encodePathPattern(pathPatternThatDoesNotNeedEscaping))
+                .isSameAs(pathPatternThatDoesNotNeedEscaping);
+    }
+}

commit 7a4807fe831534a60ee4e0fce8557754a93b2f9a

Apply `ArmeriaClientConfigurator` after the internal decorators
Motivation:

When we introduce an HTTP-based Central Dogma client implementation, we
slightly adjusted how decorators are added. As a result, the
`ArmeriaClientConfigurator` was being invoked before the internal
decorators were added.

Modifications:

- Move the common logic required for configuring an Armeria
  `ClientBuilder` to `AbstractArmeriaCentralDogmaBuilder.newClientBuilder()`.
- The customization of a `ClientBuilder` is always performed in the
  following order:
  - Builder-specific customizers are applied first.
  - `ArmeriaClientConfigurator` is applied next.
  - `ClientBuilder.clientFactory()` is set finally.
- Added a test case that verifies the customization ordering.

Result:

- Consistent customization order.
- Fixes the regression caused by 20583b1e74d583a5d5d12412e9cdb8af232f460f

client/java-armeria-legacy/src/main/java/com/linecorp/centraldogma/client/armeria/legacy/LegacyCentralDogmaBuilder.java
@@ -42,11 +42,9 @@ public class LegacyCentralDogmaBuilder extends AbstractArmeriaCentralDogmaBuilde
         final Endpoint endpoint = endpoint();
         final String scheme = "tbinary+" + (isUseTls() ? "https" : "http") + "://";
         final String uri = scheme + endpoint.authority() + "/cd/thrift/v1";
-        final ClientBuilder builder = new ClientBuilder(uri);
-        clientConfigurator().configure(builder);
-        builder.factory(clientFactory())
-               .decorator(HttpDecodingClient.newDecorator())
-               .rpcDecorator(LegacyCentralDogmaTimeoutScheduler::new);
+        final ClientBuilder builder =
+                newClientBuilder(uri, cb -> cb.decorator(HttpDecodingClient.newDecorator())
+                                              .rpcDecorator(LegacyCentralDogmaTimeoutScheduler::new));
 
         final String authorization = "Bearer " + accessToken();
         builder.decorator((delegate, ctx, req) -> {

client/java-armeria/src/main/java/com/linecorp/centraldogma/client/armeria/AbstractArmeriaCentralDogmaBuilder.java
@@ -27,10 +27,12 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Iterables;
 
+import com.linecorp.armeria.client.ClientBuilder;
 import com.linecorp.armeria.client.ClientFactory;
 import com.linecorp.armeria.client.Endpoint;
 import com.linecorp.armeria.client.endpoint.DynamicEndpointGroup;
@@ -75,14 +77,6 @@ public class AbstractArmeriaCentralDogmaBuilder<B extends AbstractArmeriaCentral
     public final B clientFactory(ClientFactory clientFactory) {
         this.clientFactory = requireNonNull(clientFactory, "clientFactory");
         return self();
-    }
-
-    /**
-     * Returns the {@link ArmeriaClientConfigurator} that will configure an underlying
-     * <a href="https://line.github.io/armeria/">Armeria</a> client which performs the actual socket I/O.
-     */
-    protected final ArmeriaClientConfigurator clientConfigurator() {
-        return clientConfigurator;
     }
 
     /**
@@ -199,5 +193,18 @@ public class AbstractArmeriaCentralDogmaBuilder<B extends AbstractArmeriaCentral
     private static Endpoint toResolvedHostEndpoint(InetSocketAddress addr) {
         return Endpoint.of(addr.getHostString(), addr.getPort())
                        .withIpAddr(addr.getAddress().getHostAddress());
+    }
+
+    /**
+     * Returns a newly created {@link ClientBuilder} configured with the specified {@code customizer}
+     * and then with the {@link ArmeriaClientConfigurator} specified with
+     * {@link #clientConfigurator(ArmeriaClientConfigurator)}.
+     */
+    protected final ClientBuilder newClientBuilder(String uri, Consumer<ClientBuilder> customizer) {
+        final ClientBuilder builder = new ClientBuilder(uri);
+        customizer.accept(builder);
+        clientConfigurator.configure(builder);
+        builder.factory(clientFactory());
+        return builder;
     }
 }

client/java-armeria/src/main/java/com/linecorp/centraldogma/client/armeria/ArmeriaCentralDogmaBuilder.java
@@ -34,10 +34,8 @@ public final class ArmeriaCentralDogmaBuilder
         final Endpoint endpoint = endpoint();
         final String scheme = "none+" + (isUseTls() ? "https" : "http") + "://";
         final String uri = scheme + endpoint.authority();
-        final ClientBuilder builder = new ClientBuilder(uri);
-        clientConfigurator().configure(builder);
-        builder.factory(clientFactory());
-        builder.decorator(HttpDecodingClient.newDecorator());
+        final ClientBuilder builder =
+                newClientBuilder(uri, cb -> cb.decorator(HttpDecodingClient.newDecorator()));
         return new ArmeriaCentralDogma(clientFactory().eventLoopGroup(),
                                        builder.build(HttpClient.class), accessToken());
     }

client/java-armeria/src/test/java/com/linecorp/centraldogma/client/armeria/ArmeriaCentralDogmaBuilderTest.java
@@ -18,12 +18,24 @@ package com.linecorp.centraldogma.client.armeria;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import java.net.URI;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.junit.Test;
 
+import com.linecorp.armeria.client.ClientBuilder;
+import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.client.ClientOptions;
 import com.linecorp.armeria.client.Endpoint;
+import com.linecorp.armeria.client.HttpClient;
 import com.linecorp.armeria.client.endpoint.EndpointGroup;
 import com.linecorp.armeria.client.endpoint.EndpointGroupRegistry;
 import com.linecorp.armeria.client.endpoint.StaticEndpointGroup;
@@ -138,6 +150,41 @@ public class ArmeriaCentralDogmaBuilderTest {
         } finally {
             EndpointGroupRegistry.unregister(groupName);
         }
+    }
+
+    /**
+     * Ensure the customization order of {@link ClientBuilder}.
+     * <ol>
+     *   <li>Builder-specified {@link Consumer}</li>
+     *   <li>User-specified {@link ArmeriaClientConfigurator}</li>
+     *   <li>User-specified {@link ClientFactory}</li>
+     * </ol>
+     */
+    @Test
+    public void newClientBuilderCustomizationOrder() {
+        final ClientFactory cf1 = mock(ClientFactory.class);
+        final ClientFactory cf2 = mock(ClientFactory.class);
+        final ClientFactory cf3 = mock(ClientFactory.class);
+        final ArmeriaCentralDogmaBuilder b = new ArmeriaCentralDogmaBuilder();
+        final StringBuilder buf = new StringBuilder();
+        b.clientFactory(cf1);
+        b.clientConfigurator(cb -> {
+            buf.append('2');
+            cb.factory(cf2);
+        });
+
+        final ClientBuilder cb = b.newClientBuilder("none+http://127.0.0.1/",
+                                                    cb2 -> {
+                                                        cb2.factory(cf3);
+                                                        buf.append('1');
+                                                    });
+        assertThat(buf.toString()).isEqualTo("12");
+
+        cb.build(HttpClient.class);
+        verify(cf1, times(1)).newClient((URI) any(), any(), (ClientOptions) any());
+        verify(cf2, never()).newClient((URI) any(), any(), (ClientOptions) any());
+        verify(cf3, never()).newClient((URI) any(), any(), (ClientOptions) any());
+        verifyNoMoreInteractions(cf1, cf2, cf3);
     }
 
     private static final class ArmeriaCentralDogmaBuilder