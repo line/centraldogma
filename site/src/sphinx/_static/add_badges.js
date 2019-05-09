a6a099abe148a769d431b1f54de8595d3f950c0b
Trustin Lee <trustin@linecorp.com>
2019-05-09 23:01:03 +0900

    Improve the overall look and feel of the web site (#397)
    Motivation:
    
    It's always better looking nicer.
    
    Modifications:
    
    - Typography
      - Increased the overall font size and line height a little bit for
        readability.
      - Changed the heading font from Source Sans Pro to Merriweather
        because it is more distinguished from body text.
      - Changed the body font from Source Sans Pro to Lato because Source
        Sans Pro is relatively smaller and denser than other sans-serif
        fonts, making the CSS customization harder.
        - Lato is the default font used by the Sphinx theme we are using,
          which means one less thing to worry.
      - Changed the monospace font from Inconsolata to Hack because:
        - Inconsolata looks thin when paired with Lato.
        - Hack is my favorite font. :-)
    - Layout
      - Increased the width of the content pane from 800px to 960px.
      - Made the content pane resizable by dragging its left or right edge.
    - Badges
      - Replaced the Slack badge with a custom badge from Shields.io,
        simplifying `add_badges.js`.
      - Used `<object>` tag instead of `<img>` tag so that the badges are
        unaffected by our CSS and the hyperlinks embedded in the `.svg`
        images are preserved.
      - Added more badges:
        - GitHub stars
        - Commit activity
        - Good first issues
    - Dependencies
      - gh-fork-ribbon 0.1.1 -> 0.2.2
      - highlightjs-line-numbers.js 2.6.0 -> 2.7.0
      - sphinx-gradle-plugin 2.3.1 -> 2.4.0
    - Miscellaneous
      - `0px` -> `0`
      - Cleaned up code block and literal styles
      - Fixed a bug where wrong fonts were being used in the search bar and
        prev/next buttons.
    
    Result:
    
    - Looking better.

--- site/src/sphinx/_static/add_badges.js
+++ site/src/sphinx/_static/add_badges.js
@@ -1,38 +1,40 @@
-// Append the Slack invitation and project badges at the end of the menu (sidenav).
-function addSlackInvitation(parent) {
-  var li = document.createElement('li');
-  li.className = 'toctree-l1';
-  var div = document.createElement('div');
-  div.className = 'slack-invitation';
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://line-slacknow.herokuapp.com/central-dogma/slackin.js';
-  div.appendChild(script);
-  li.appendChild(div);
-  parent.appendChild(li);
-}
-
+// Append the project badges at the end of the menu (sidenav).
 function addBadge(parent, src, href) {
-  var img = document.createElement('img');
-  img.src = src;
-  var a = document.createElement('a');
-  a.href = href;
-  a.appendChild(img);
-  parent.appendChild(a);
+  var obj = document.createElement('object');
+  obj.data = src;
+
+  if (typeof href === 'string') {
+    obj.style.pointerEvents = 'none';
+    var a = document.createElement('a');
+    a.href = href;
+    a.target = '_blank';
+    a.rel = 'noopener';
+    a.appendChild(obj);
+    parent.appendChild(a);
+  } else {
+    parent.appendChild(obj);
+  }
   parent.appendChild(document.createElement('br'));
 }
+
 
 function addBadges(parent) {
   var li = document.createElement('li');
   li.className = 'toctree-l1';
   var div = document.createElement('div');
   div.className = 'project-badges';
+  addBadge(div, 'https://img.shields.io/github/stars/line/centraldogma.svg?style=social');
+  addBadge(div, 'https://img.shields.io/badge/chat-on%20slack-brightgreen.svg?style=social',
+    'https://join.slack.com/t/central-dogma/shared_invite/enQtNjA5NDk5MTExODQzLWRlZTZmZDljNDY3OTBmN2Y1NDAwMGUzMTMzNmFlOTYzNzYyNDNmNmM5YTk3YmExM2M4NGRkOTY2NTE1MDJhODY');
   addBadge(div, 'https://img.shields.io/travis/line/centraldogma/master.svg?style=flat-square',
     'https://travis-ci.org/line/centraldogma');
   addBadge(div, 'https://img.shields.io/appveyor/ci/line/centraldogma/master.svg?label=appveyor&style=flat-square',
     'https://ci.appveyor.com/project/line/centraldogma/branch/master');
   addBadge(div, 'https://img.shields.io/maven-central/v/com.linecorp.centraldogma/centraldogma-common.svg?style=flat-square',
-    'https://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.linecorp.centraldogma%22');
+    'https://search.maven.org/search?q=g:com.linecorp.centraldogma');
+  addBadge(div, 'https://img.shields.io/github/commit-activity/m/line/centraldogma.svg?style=flat-square',
+    'https://github.com/line/centraldogma/pulse');
+  addBadge(div, 'https://img.shields.io/github/issues/line/centraldogma/good-first-issue.svg?label=good%20first%20issues&style=flat-square');
   addBadge(div, 'https://img.shields.io/codecov/c/github/line/centraldogma/master.svg?style=flat-square',
     'https://codecov.io/gh/line/centraldogma');
   li.appendChild(div);
@@ -44,7 +46,6 @@ if (menus.length > 0) {
   var menu = menus[0];
   var lists = menu.getElementsByTagName('ul');
   if (lists.length > 0) {
-    addSlackInvitation(lists[0]);
     addBadges(lists[0]);
   }
 }
