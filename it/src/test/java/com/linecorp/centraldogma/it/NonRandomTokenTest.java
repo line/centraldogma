4ac5e3a2b2d5ef1d3340cb7c8ff1915b57d36eee
Ikhun Um <ih.pert@gmail.com>
2021-01-19 15:19:59 +0900

    Allow creating a new token from the input string for admin (#549)
    Motivation:
    
    When migrating a Central Dogma into another Central Dogma, a repository could be easily migrated by copying its file folder under `/data`.
    However, it is difficult to merge the tokens of two systems because all tokens are managed by `tokens` repository.
    
    Modifications:
    
    - Allow creating a new token from `sceret` parameter for admin
    - Increase timeout of `ZooKeeperCommandExecutorTest.hierarchicalQuorumsWithFailOver()` for receiving flakiness
    - 
    Result:
    
    - You can now create a new token with non-random string starting with `appToken-` for admin
    - Fixes #550

---
+++ it/src/test/java/com/linecorp/centraldogma/it/NonRandomTokenTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.centraldogma.it;
+
+import static com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil.login;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.auth.OAuth2Token;
+import com.linecorp.centraldogma.internal.Jackson;
+import com.linecorp.centraldogma.internal.api.v1.AccessToken;
+import com.linecorp.centraldogma.server.CentralDogmaBuilder;
+import com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil;
+import com.linecorp.centraldogma.testing.internal.auth.TestAuthProviderFactory;
+import com.linecorp.centraldogma.testing.junit.CentralDogmaExtension;
+
+class NonRandomTokenTest {
+
+    @RegisterExtension
+    static final CentralDogmaExtension dogma = new CentralDogmaExtension() {
+        @Override
+        protected void configure(CentralDogmaBuilder builder) {
+            builder.administrators(TestAuthMessageUtil.USERNAME);
+            builder.authProviderFactory(new TestAuthProviderFactory());
+        }
+    };
+
+    @Test
+    void createNonRandomToken() throws Exception {
+        final WebClient client = dogma.httpClient();
+        final AggregatedHttpResponse response = login(client,
+                                                      TestAuthMessageUtil.USERNAME,
+                                                      TestAuthMessageUtil.PASSWORD);
+
+        assertThat(response.status()).isEqualTo(HttpStatus.OK);
+        final String sessionId = Jackson.readValue(response.content().array(), AccessToken.class)
+                                        .accessToken();
+        final WebClient adminClient = WebClient.builder(client.uri())
+                                               .auth(OAuth2Token.of(sessionId)).build();
+
+        final HttpRequest request = HttpRequest.builder()
+                                               .post("/api/v1/tokens")
+                                               .content(MediaType.FORM_DATA,
+                                                        "secret=appToken-secret&isAdmin=true&appId=foo")
+                                               .build();
+        AggregatedHttpResponse res = adminClient.execute(request).aggregate().join();
+        assertThat(res.status()).isEqualTo(HttpStatus.CREATED);
+        res = adminClient.get("/api/v1/tokens").aggregate().join();
+        assertThat(res.contentUtf8()).contains("\"secret\":\"appToken-secret\"");
+    }
+}
--- server/src/test/java/com/linecorp/centraldogma/server/internal/api/TokenServiceTest.java
+++ server/src/test/java/com/linecorp/centraldogma/server/internal/api/TokenServiceTest.java
@@ -63,10 +63,10 @@ class TokenServiceTest {
 
     @Test
     void adminToken() {
-        final Token token = tokenService.createToken("forAdmin1", true, adminAuthor, admin).join()
+        final Token token = tokenService.createToken("forAdmin1", true, null, adminAuthor, admin).join()
                                         .content();
         assertThat(token.isActive()).isTrue();
-        assertThatThrownBy(() -> tokenService.createToken("forAdmin2", true, guestAuthor, guest)
+        assertThatThrownBy(() -> tokenService.createToken("forAdmin2", true, null, guestAuthor, guest)
                                              .join())
                 .isInstanceOf(IllegalArgumentException.class);
 
@@ -87,9 +87,9 @@ class TokenServiceTest {
 
     @Test
     void userToken() {
-        final Token userToken1 = tokenService.createToken("forUser1", false, adminAuthor, admin)
+        final Token userToken1 = tokenService.createToken("forUser1", false, null, adminAuthor, admin)
                                              .join().content();
-        final Token userToken2 = tokenService.createToken("forUser2", false, guestAuthor, guest)
+        final Token userToken2 = tokenService.createToken("forUser2", false, null, guestAuthor, guest)
                                              .join().content();
         assertThat(userToken1.isActive()).isTrue();
         assertThat(userToken2.isActive()).isTrue();
@@ -110,5 +110,22 @@ class TokenServiceTest {
             assertThat(t.creation()).isEqualTo(userToken2.creation());
             assertThat(t.deactivation()).isEqualTo(userToken2.deactivation());
         });
+    }
+
+    @Test
+    void nonRandomToken() {
+        final Token token = tokenService.createToken("forAdmin1", true, "appToken-secret", adminAuthor,
+                                                     admin)
+                                        .join().content();
+        assertThat(token.isActive()).isTrue();
+
+        final Collection<Token> tokens = tokenService.listTokens(admin).join();
+        assertThat(tokens.stream().filter(t -> !StringUtil.isNullOrEmpty(t.secret()))).hasSize(1);
+
+        assertThatThrownBy(() -> tokenService.createToken("forUser1", true, "appToken-secret", guestAuthor,
+                                                          guest)
+                                             .join())
+                .isInstanceOf(IllegalArgumentException.class);
+        tokenService.deleteToken(ctx, "forAdmin1", adminAuthor, admin).join();
     }
 }
