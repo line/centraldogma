f01a4016e1d3351a9d1933298df3e6577bb9d58d
minwoox <songmw725@gmail.com>
2021-07-06 21:59:04 +0900

    Use Git repository format version 1 Motivation: Releated: #83 We use Git repository format version 1 from 0.19.0 I think we can force the version 1 and throw an exception when CD tries to open a Git repository whose format version is 0.
    Modifications:
    - Throw an exception if CD tries to open a Git repository whose format version is 0.
    
    Result:
    - Simplicity

--- server/src/main/java/com/linecorp/centraldogma/server/internal/storage/repository/git/GitRepositoryManager.java
+++ server/src/main/java/com/linecorp/centraldogma/server/internal/storage/repository/git/GitRepositoryManager.java
@@ -48,22 +48,15 @@ public class GitRepositoryManager extends DirectoryBasedStorageManager<Repositor
     private static final Logger logger = LoggerFactory.getLogger(GitRepositoryManager.class);
 
     private final Project parent;
-    private final GitRepositoryFormat preferredFormat;
     private final Executor repositoryWorker;
+
+    @Nullable
     private final RepositoryCache cache;
 
     public GitRepositoryManager(Project parent, File rootDir, Executor repositoryWorker,
                                 Executor purgeWorker, @Nullable RepositoryCache cache) {
-        this(parent, rootDir, GitRepositoryFormat.V1, repositoryWorker, purgeWorker, cache);
-    }
-
-    public GitRepositoryManager(Project parent, File rootDir, GitRepositoryFormat preferredFormat,
-                                Executor repositoryWorker, Executor purgeWorker,
-                                @Nullable RepositoryCache cache) {
-
         super(rootDir, Repository.class, purgeWorker);
         this.parent = requireNonNull(parent, "parent");
-        this.preferredFormat = requireNonNull(preferredFormat, "preferredFormat");
         this.repositoryWorker = requireNonNull(repositoryWorker, "repositoryWorker");
         this.cache = cache;
         init();
@@ -76,51 +69,7 @@ public class GitRepositoryManager extends DirectoryBasedStorageManager<Repositor
 
     @Override
     protected Repository openChild(File childDir) throws Exception {
-        final GitRepository repository = new GitRepository(parent, childDir, repositoryWorker, cache);
-        if (repository.needsMigration(preferredFormat)) {
-            return migrate(childDir, parent, repositoryWorker, repository, preferredFormat, cache);
-        } else {
-            return repository;
-        }
-    }
-
-    private static Repository migrate(File childDir, Project project, Executor repositoryWorker,
-                                      GitRepository oldRepo, GitRepositoryFormat newFormat,
-                                      RepositoryCache cache) throws IOException {
-        boolean closedOldRepo = false;
-        try {
-            logger.info("Migrating from {} to {}: {}", oldRepo.format(), newFormat, oldRepo);
-            final File newChildDir = new File(childDir.getParentFile(),
-                                              "_newfmt_" + childDir.getName());
-            final File oldChildDir = new File(childDir.getParentFile(),
-                                              "_oldfmt_" + childDir.getName());
-
-            if (newChildDir.exists()) {
-                deleteCruft(newChildDir);
-            }
-            if (oldChildDir.exists()) {
-                deleteCruft(oldChildDir);
-            }
-
-            oldRepo.cloneTo(newChildDir, newFormat, new MigrationProgressLogger(oldRepo));
-            oldRepo.internalClose();
-            closedOldRepo = true;
-
-            if (!childDir.renameTo(oldChildDir)) {
-                throw new IOException("failed to rename " + childDir + " to " + oldChildDir);
-            }
-            if (!newChildDir.renameTo(childDir)) {
-                throw new IOException("failed to rename " + newChildDir + " to " + childDir);
-            }
-
-            final GitRepository newRepo = new GitRepository(project, childDir, repositoryWorker, cache);
-            logger.info("Migrated from {} to {}: {}", oldRepo.format(), newFormat, newRepo);
-            return newRepo;
-        } finally {
-            if (!closedOldRepo) {
-                oldRepo.internalClose();
-            }
-        }
+        return new GitRepository(parent, childDir, repositoryWorker, cache);
     }
 
     private static void deleteCruft(File dir) throws IOException {
@@ -131,7 +80,7 @@ public class GitRepositoryManager extends DirectoryBasedStorageManager<Repositor
 
     @Override
     protected Repository createChild(File childDir, Author author, long creationTimeMillis) throws Exception {
-        return new GitRepository(parent, childDir, preferredFormat, repositoryWorker,
+        return new GitRepository(parent, childDir, repositoryWorker,
                                  creationTimeMillis, author, cache);
     }
 
