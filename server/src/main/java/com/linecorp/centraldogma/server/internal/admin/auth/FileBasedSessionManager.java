ba4c2083fe6de673515802c1d5bbb00a4f6174f1
lujie <lujie@ict.ac.cn>
2021-10-14 10:55:25 +0800

    fix resource leak due to Files.walk

--- server/src/main/java/com/linecorp/centraldogma/server/internal/admin/auth/FileBasedSessionManager.java
+++ server/src/main/java/com/linecorp/centraldogma/server/internal/admin/auth/FileBasedSessionManager.java
@@ -287,9 +287,9 @@ public final class FileBasedSessionManager implements SessionManager {
             try {
                 logger.debug("Started {} job.", ExpiredSessionDeletingJob.class.getSimpleName());
                 final Path rootDir = (Path) context.getJobDetail().getJobDataMap().get(ROOT_DIR);
-                final Instant now = Instant.now();
-                Files.walk(rootDir, 2)
-                     .filter(FileBasedSessionManager::isSessionFile)
+		final Instant now = Instant.now();
+                try (Stream<Path> stream = Files.walk(rootDir, 2)) {
+                     stream.filter(FileBasedSessionManager::isSessionFile)
                      .map(path -> {
                          try {
                              return Jackson.readValue(Files.readAllBytes(path), Session.class);
@@ -319,7 +319,7 @@ public final class FileBasedSessionManager implements SessionManager {
                          } catch (Throwable cause) {
                              logger.warn("Failed to delete an expired session: {}", path, cause);
                          }
-                     });
+                     });}
                 logger.debug("Finished {} job.", ExpiredSessionDeletingJob.class.getSimpleName());
             } catch (Throwable cause) {
                 logger.warn("Failed {} job:", ExpiredSessionDeletingJob.class.getSimpleName(), cause);
