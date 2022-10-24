629b12f8f319f542e06926a08e194109770c45ae
minwoox <songmw725@gmail.com>
2022-10-24 11:05:40 +0900

    Address the comment by @ikhoon

--- server/src/main/java/com/linecorp/centraldogma/server/internal/replication/ZooKeeperCommandExecutor.java
+++ server/src/main/java/com/linecorp/centraldogma/server/internal/replication/ZooKeeperCommandExecutor.java
@@ -901,7 +901,7 @@ public final class ZooKeeperCommandExecutor
         final QuotaConfig writeQuota = writeLock.writeQuota;
         if (lease == null) {
             safeRelease(mtx);
-            throw new TooManyRequestsException("commits", executionPath, writeQuota.requestQuota());
+            throw new TooManyRequestsException("commits", executionPath, writeQuota.permitsPerSecond());
         } else {
             quotaExecutor.schedule(() -> writeLock.semaphore.returnLease(lease),
                                    writeQuota.timeWindowSeconds(), TimeUnit.SECONDS);
