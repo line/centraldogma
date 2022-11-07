756708100a61aed9eeb43ee23da3082b76d29dda
Ikhun Um <ih.pert@gmail.com>
2022-11-07 16:04:06 +0900

    Implement a basic skeleton using Redux Tookit and other tools (#740)
    Motivation:
    
    The next step of #722
    
    Modifications:
    
    - Used https://redux-toolkit.js.org/ for state management and data fetch. Following links would be helpful to understand the current architecture.
      - https://redux-toolkit.js.org/tutorials/typescript
      - https://redux-toolkit.js.org/api/createAsyncThunk
      - https://redux-toolkit.js.org/tutorials/rtk-query
    - Structured code with "Feature folders"-style https://redux.js.org/faq/code-structure#what-should-my-file-structure-look-like-how-should-i-group-my-action-creators-and-reducers-in-my-project-where-should-my-selectors-go
    - Used https://github.com/salvoravida/redux-first-history for easily mananging history with react-router and redux.
    - Used https://formik.org/ to create the built-in login form.
      - Formik will be used for the mirror settings page and other input forms later.
    - Chakra UI and its theme is used to design the web application.
    
    Implementation-Note:
    
    - Relative paths to tsconfig.json is used for consistent imports. 
      ```ts 
      // Good: 
      import { getSessionId } from 'dogma/features/auth/Authorized'; 
      import { ProjectDto } from 'dogma/features/project/ProjectDto';
    
      // Bad: 
      import { getSessionId } from '../auth/Authorized'; 
      import { ProjectDto } from '../project/ProjectDto'; 
      ```
      To automatically import the paths relative to the root, enable `Editor` -> `Code Style` -> `Typescript` -> `Imports` -> `Use paths relative to tsconfig.json`
    - WebStorm or the cutting edge typescript plugin of VScode correctly infers the types of Redux tool kits. However, the typescript plugin of IntelliJ IDEA doesn't seem to infer `createApi` of RTK query. https://github.com/reduxjs/redux-toolkit/issues/2320#issuecomment-1125688867
    
    Result:
    
    We can now login to Central Dogma and see the project list with the new web application.

---
+++ webapp/src/test/java/com/linecorp/centraldogma/webapp/ShiroCentralDogmaTestServer.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.centraldogma.webapp;
+
+import static com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil.PASSWORD;
+import static com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil.USERNAME;
+import static com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil.login;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.shiro.config.Ini;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.SessionProtocol;
+import com.linecorp.centraldogma.client.armeria.ArmeriaCentralDogmaBuilder;
+import com.linecorp.centraldogma.internal.Jackson;
+import com.linecorp.centraldogma.internal.api.v1.AccessToken;
+import com.linecorp.centraldogma.server.CentralDogma;
+import com.linecorp.centraldogma.server.CentralDogmaBuilder;
+import com.linecorp.centraldogma.server.auth.shiro.ShiroAuthProviderFactory;
+
+final class ShiroCentralDogmaTestServer {
+
+    private static final int PORT = 36462;
+
+    /**
+     * Start a new Central Dogma server with Apache Shiro.
+     */
+    @SuppressWarnings("UncommentedMain")
+    public static void main(String[] args) throws IOException {
+        final Path rooDir = Files.createTempDirectory("dogma-test");
+        final CentralDogma server = new CentralDogmaBuilder(rooDir.toFile())
+                // Enable the legacy webapp
+                // .webAppEnabled(true)
+                .port(PORT, SessionProtocol.HTTP)
+                .authProviderFactory(new ShiroAuthProviderFactory(unused -> {
+                    final Ini iniConfig = new Ini();
+                    iniConfig.addSection("users").put(USERNAME, PASSWORD);
+                    return iniConfig;
+                }))
+                .build();
+        server.start().join();
+        scaffold();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::close));
+    }
+
+    private static void scaffold() throws UnknownHostException, JsonProcessingException {
+        final com.linecorp.centraldogma.client.CentralDogma client = new ArmeriaCentralDogmaBuilder()
+                .host("127.0.0.1", PORT)
+                .accessToken(getSessionToken())
+                .build();
+        client.createProject("foo").join();
+        client.createRepository("foo", "bar").join();
+    }
+
+    private static String getSessionToken() throws JsonProcessingException {
+        final WebClient client = WebClient.of("http://127.0.0.1:" + PORT);
+        final AggregatedHttpResponse response = login(client, USERNAME, PASSWORD);
+        // Ensure authorization works.
+        final AccessToken accessToken = Jackson.readValue(response.contentUtf8(), AccessToken.class);
+        return accessToken.accessToken();
+    }
+
+    private ShiroCentralDogmaTestServer() {}
+}
--- webapp/webpack.config.ts
+++ webapp/webpack.config.ts
@@ -3,48 +3,65 @@ import webpack from 'webpack';
 import 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+const centralDogmaPort = process.env.CENTRALDOGMA_PORT || '36462';
 const isDev = !!process.env.WEBPACK_DEV;
 
 const config: webpack.Configuration = {
-    mode: isDev ? 'development' : 'production',
-    devtool: isDev ? 'inline-source-map' : undefined,
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            hash: true,
-        }),
+  mode: isDev ? 'development' : 'production',
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './build/web'),
-        chunkFilename: '[id].[chunkhash].js',
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    devServer: {
-        static: './dist',
-        client: {
-            overlay: {
-                warnings: false,
-                errors: true,
-            },
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true,
+    }),
+  ],
+  output: {
+    publicPath: '/',
+    filename: 'asserts/[name].js',
+    path: path.resolve(__dirname, './build/web'),
+    chunkFilename: '[id].[chunkhash].js',
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: '/',
+    port: 3000,
+    proxy: [
+      {
+        context: (pathname) => {
+          return (
+            pathname !== '/' && ['/app', '/assets', '/web/auth'].every((prefix) => !pathname.startsWith(prefix))
+          );
         },
+        target: `http://localhost:${centralDogmaPort}`,
+        changeOrigin: true,
+      },
+    ],
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
     },
+  },
 };
 
 export default config;
