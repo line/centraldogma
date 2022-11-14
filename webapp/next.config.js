bcc76d165d96547c768422cce239c563c3a04ca7
rainy789 <myliving21@yahoo.co.id>
2022-11-14 13:09:39 +0700

    Redirect homepage to project page

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,5 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-
-module.exports = withBundleAnalyzer({});
+let nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/app/projects',
+        permanent: true,
+      },
+    ];
+  },
+};
+module.exports = withBundleAnalyzer(nextConfig);
