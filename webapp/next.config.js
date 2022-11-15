c4738c2533636b03360496239ac3721ad2d04b9f
rainy789 <myliving21@yahoo.co.id>
2022-11-15 07:16:42 +0700

    Clean up login page

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,15 +1,4 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-let nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/app/projects',
-        permanent: true,
-      },
-    ];
-  },
-};
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withBundleAnalyzer({});
--- webapp/pages/web/auth/login.tsx
+++ webapp/pages/web/auth/login.tsx
@@ -1,15 +1,24 @@
 import Head from 'next/head';
 import { LoginForm } from 'dogma/features/auth/LoginForm';
 import { useAppSelector } from 'dogma/store';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 const LoginPage = () => {
+  const router = useRouter();
   const user = useAppSelector((state) => state.auth.user);
+  useEffect(() => {
+    if (user) {
+      router.push('/app/projects');
+    }
+  }, [router, user]);
   return (
     <>
       <Head>
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="Central Dogma | Login" content="Login ..." />
       </Head>
-      {user ? <h1>You are logged in.</h1> : <LoginForm />}
+      <LoginForm />
     </>
   );
 };
