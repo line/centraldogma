92bf444df66e4d865f76e5c092bf31dbcd1abc4b
Ikhun Um <ih.pert@gmail.com>
2022-11-18 21:37:21 +0900

    Address comments by @rainy789

--- webapp/src/dogma/features/auth/Authorized.tsx
+++ webapp/src/dogma/features/auth/Authorized.tsx
@@ -49,7 +49,7 @@ export const Authorized = (props: { children: ReactNode }) => {
   }
   if (typeof window !== 'undefined' && !auth.isAuthenticated) {
     if (process.env.NEXT_PUBLIC_HOST) {
-      router.push(`${process.env.NEXT_PUBLIC_HOST}/link/auth/login/?return_to=http://127.0.0.1:3000`);
+      router.push(`${process.env.NEXT_PUBLIC_HOST}/link/auth/login/?return_to=${window.location.origin}`);
     } else {
       router.push(`/link/auth/login/`);
     }
