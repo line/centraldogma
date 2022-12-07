62b3147394fc3636f83fd25aa0035419fb3da437
rainy789 <rainyxyz07@gmail.com>
2022-12-07 16:09:20 +0700

    Update mock apis

---
+++ webapp/src/pages/api/v1/projects/[projectName]/repos/[repoName]/list.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { FileDto } from 'dogma/features/file/FileDto';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faker } from '@faker-js/faker';
+
+const newFile = (id: number): FileDto => {
+  return {
+    revision: faker.datatype.number({
+      min: 1,
+      max: 10,
+    }),
+    path: `/${id}-${faker.animal.rabbit().replaceAll(' ', '-').toLowerCase()}`,
+    type: 'TEXT',
+    url: faker.internet.url(),
+  };
+};
+const fileList: FileDto[] = [];
+const makeData = (len: number) => {
+  for (let i = 0; i < len; i++) {
+    fileList.push(newFile(i));
+  }
+};
+makeData(20);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { fileItem } = req.body;
+  const { query } = req;
+  const { revision } = query;
+  const revisionNumber = parseInt(revision as string);
+  const filtered = isNaN(revisionNumber)
+    ? fileList
+    : fileList.filter((file: FileDto) => file.revision == revisionNumber);
+  switch (req.method) {
+    case 'GET':
+      res.status(200).json(filtered);
+      break;
+    case 'POST':
+      fileList.push(fileItem);
+      res.status(200).json(fileList);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
+  }
+}
---
+++ webapp/src/pages/api/v1/projects/[projectName]/temp.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const repoDetail = {
+  name: 'abcd',
+  repos: {
+    meta: {
+      name: 'meta',
+      perRolePermissions: { owner: ['READ', 'WRITE'], member: [], guest: [] },
+      perUserPermissions: {},
+      perTokenPermissions: {},
+      creation: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-23T03:13:50.129903Z' },
+    },
+    repo1: {
+      name: 'repo1',
+      perRolePermissions: { owner: ['READ', 'WRITE'], member: ['READ', 'WRITE'], guest: [] },
+      perUserPermissions: {},
+      perTokenPermissions: {},
+      creation: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-23T03:16:18.853509Z' },
+    },
+    repo2: {
+      name: 'repo2',
+      perRolePermissions: { owner: ['READ', 'WRITE'], member: ['READ', 'WRITE'], guest: [] },
+      perUserPermissions: {},
+      perTokenPermissions: {},
+      creation: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-28T03:01:48.202144Z' },
+      removal: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-29T04:16:12.870155Z' },
+    },
+  },
+  members: {
+    'lz123456@localhost.localdomain': {
+      login: 'lz123456@localhost.localdomain',
+      role: 'OWNER',
+      creation: { user: 'lz123456@localhost.localdomain', timestamp: '2022-11-30T02:43:04.655753Z' },
+    },
+    'lb56789@localhost.localdomain': {
+      login: 'lb56789@localhost.localdomain',
+      role: 'MEMBER',
+      creation: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-23T03:13:50.129903Z' },
+    },
+  },
+  tokens: {},
+  creation: { user: 'lb56789@localhost.localdomain', timestamp: '2022-11-23T03:13:50.129903Z' },
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      res.status(200).json(repoDetail);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
+  }
+}
