0fc40c7f89694be4434aa0a627ad975d3eee5a25
clavinjune <24659468+clavinjune@users.noreply.github.com>
2022-12-24 12:51:25 +0700

    add tests for handleCopyApiUrl and handleCopyAsCurlCommand
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- webapp/__tests__/dogma/feature/file/FileList.test.tsx
+++ webapp/__tests__/dogma/feature/file/FileList.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import FileList, { FileListProps } from 'dogma/features/file/FileList';
 import { FileDto } from 'dogma/features/file/FileDto';
 
@@ -37,6 +37,8 @@ describe('FileList', () => {
       data: mockfileList,
       projectName: 'ProjectAlpha',
       repoName: 'repo1',
+      handleCopyApiUrl: jest.fn(),
+      handleCopyAsCurlCommand: jest.fn(),
     };
   });
 
@@ -72,5 +74,19 @@ describe('FileList', () => {
       'href',
       `/app/projects/${expectedProps.projectName}/repos/${expectedProps.repoName}/files/head${firstFileName}`,
     );
+  });
+
+  it('calls handleCopyApiUrl when copy API URL button is clicked', () => {
+    const { getAllByText } = render(<FileList {...expectedProps} />);
+    const firstButton = getAllByText('Copy API URL', { selector: 'button' })[0];
+    fireEvent.click(firstButton);
+    expect(expectedProps.handleCopyApiUrl).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls handleCopyAsCurlCommand when copy as a curl command button is clicked', () => {
+    const { getAllByText } = render(<FileList {...expectedProps} />);
+    const firstButton = getAllByText('Copy as a curl command', { selector: 'button' })[0];
+    fireEvent.click(firstButton);
+    expect(expectedProps.handleCopyAsCurlCommand).toHaveBeenCalledTimes(1);
   });
 });
