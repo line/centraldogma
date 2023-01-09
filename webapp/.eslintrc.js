e08596d231361b7d340494d40117be8caf26ffc5
rainy789 <rainyxyz07@gmail.com>
2023-01-09 13:18:39 +0700

    Update eslint config

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -1,10 +1,20 @@
-const defaultConfig = require('../settings/eslint/eslintrc');
-
 module.exports = {
-  ...defaultConfig,
-  ignorePatterns: [...defaultConfig.ignorePatterns, '.next/'],
-  extends: ['next', 'plugin:@next/next/recommended'],
-  parserOptions: {
-    project: './tsconfig.json',
+  env: {
+    browser: true,
+    es2021: true,
   },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
 };
