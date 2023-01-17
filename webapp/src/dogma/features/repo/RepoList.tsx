b2901bdf05ce16fd7527123317f0f84da4432565
rainy789 <rainyxyz07@gmail.com>
2023-01-17 08:30:42 +0700

    Memoize repo tables

--- webapp/src/dogma/features/repo/RepoList.tsx
+++ webapp/src/dogma/features/repo/RepoList.tsx
@@ -5,6 +5,7 @@ import { ChakraLink } from 'dogma/common/components/ChakraLink';
 import { DateWithTooltip } from 'dogma/common/components/DateWithTooltip';
 import { DynamicDataTable } from 'dogma/common/components/table/DynamicDataTable';
 import { RepoDto } from 'dogma/features/repo/RepoDto';
+import { useMemo } from 'react';
 import { RiGitRepositoryFill } from 'react-icons/ri';
 
 export type RepoListProps<Data extends object> = {
@@ -14,53 +15,56 @@ export type RepoListProps<Data extends object> = {
 
 const RepoList = <Data extends object>({ data, projectName }: RepoListProps<Data>) => {
   const columnHelper = createColumnHelper<RepoDto>();
-  const columns = [
-    columnHelper.accessor((row: RepoDto) => row.name, {
-      cell: (info) => (
-        <ChakraLink
-          fontWeight={'semibold'}
-          href={`/app/projects/${projectName}/repos/${info.getValue()}/list/head`}
-        >
-          <HStack>
-            <Box>
-              <RiGitRepositoryFill />
-            </Box>
-            <Box>{info.getValue()}</Box>
-          </HStack>
-        </ChakraLink>
-      ),
-      header: 'Name',
-    }),
-    columnHelper.accessor((row: RepoDto) => row.creator.name, {
-      cell: (info) => info.getValue(),
-      header: 'Creator',
-    }),
-    columnHelper.accessor((row: RepoDto) => row.createdAt, {
-      cell: (info) => <DateWithTooltip date={info.getValue()} />,
-      header: 'Created',
-    }),
-    columnHelper.accessor((row: RepoDto) => row.headRevision, {
-      // TODO: Show the commit message of HEAD revision
-      cell: (info) => info.getValue(),
-      header: 'HEAD',
-      meta: {
-        isNumeric: true,
-      },
-    }),
-    columnHelper.accessor((row: RepoDto) => row.name, {
-      cell: () => (
-        <Wrap>
-          <WrapItem>
-            <Button leftIcon={<DeleteIcon />} colorScheme="red" size="sm">
-              Delete
-            </Button>
-          </WrapItem>
-        </Wrap>
-      ),
-      header: 'Actions',
-      enableSorting: false,
-    }),
-  ];
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor((row: RepoDto) => row.name, {
+        cell: (info) => (
+          <ChakraLink
+            fontWeight={'semibold'}
+            href={`/app/projects/${projectName}/repos/${info.getValue()}/list/head`}
+          >
+            <HStack>
+              <Box>
+                <RiGitRepositoryFill />
+              </Box>
+              <Box>{info.getValue()}</Box>
+            </HStack>
+          </ChakraLink>
+        ),
+        header: 'Name',
+      }),
+      columnHelper.accessor((row: RepoDto) => row.creator.name, {
+        cell: (info) => info.getValue(),
+        header: 'Creator',
+      }),
+      columnHelper.accessor((row: RepoDto) => row.createdAt, {
+        cell: (info) => <DateWithTooltip date={info.getValue()} />,
+        header: 'Created',
+      }),
+      columnHelper.accessor((row: RepoDto) => row.headRevision, {
+        // TODO: Show the commit message of HEAD revision
+        cell: (info) => info.getValue(),
+        header: 'HEAD',
+        meta: {
+          isNumeric: true,
+        },
+      }),
+      columnHelper.accessor((row: RepoDto) => row.name, {
+        cell: () => (
+          <Wrap>
+            <WrapItem>
+              <Button leftIcon={<DeleteIcon />} colorScheme="red" size="sm">
+                Delete
+              </Button>
+            </WrapItem>
+          </Wrap>
+        ),
+        header: 'Actions',
+        enableSorting: false,
+      }),
+    ],
+    [columnHelper, projectName],
+  );
   return <DynamicDataTable columns={columns as ColumnDef<Data>[]} data={data} />;
 };
 
--- webapp/src/dogma/features/repo/RepoTokenList.tsx
+++ webapp/src/dogma/features/repo/RepoTokenList.tsx
@@ -5,6 +5,7 @@ import { DateWithTooltip } from 'dogma/common/components/DateWithTooltip';
 import { UserRole } from 'dogma/common/components/UserRole';
 import { DynamicDataTable } from 'dogma/common/components/table/DynamicDataTable';
 import { RepoTokenDetailDto } from 'dogma/features/repo/RepoTokenDto';
+import { useMemo } from 'react';
 
 export type RepoTokenListProps<Data extends object> = {
   data: Data[];
@@ -12,33 +13,36 @@ export type RepoTokenListProps<Data extends object> = {
 
 const RepoTokenList = <Data extends object>({ data }: RepoTokenListProps<Data>) => {
   const columnHelper = createColumnHelper<RepoTokenDetailDto>();
-  const columns = [
-    columnHelper.accessor((row: RepoTokenDetailDto) => row.appId, {
-      cell: (info) => info.getValue(),
-      header: 'App ID',
-    }),
-    columnHelper.accessor((row: RepoTokenDetailDto) => row.role, {
-      cell: (info) => <UserRole role={info.getValue()} />,
-      header: 'Role',
-    }),
-    columnHelper.accessor((row: RepoTokenDetailDto) => row.creation.user, {
-      cell: (info) => info.getValue(),
-      header: 'Added By',
-    }),
-    columnHelper.accessor((row: RepoTokenDetailDto) => row.creation.timestamp, {
-      cell: (info) => <DateWithTooltip date={info.getValue()} />,
-      header: 'Added At',
-    }),
-    columnHelper.accessor((row: RepoTokenDetailDto) => row.appId, {
-      cell: () => (
-        <Button leftIcon={<DeleteIcon />} size="sm" colorScheme="red">
-          Delete
-        </Button>
-      ),
-      header: 'Actions',
-      enableSorting: false,
-    }),
-  ];
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor((row: RepoTokenDetailDto) => row.appId, {
+        cell: (info) => info.getValue(),
+        header: 'App ID',
+      }),
+      columnHelper.accessor((row: RepoTokenDetailDto) => row.role, {
+        cell: (info) => <UserRole role={info.getValue()} />,
+        header: 'Role',
+      }),
+      columnHelper.accessor((row: RepoTokenDetailDto) => row.creation.user, {
+        cell: (info) => info.getValue(),
+        header: 'Added By',
+      }),
+      columnHelper.accessor((row: RepoTokenDetailDto) => row.creation.timestamp, {
+        cell: (info) => <DateWithTooltip date={info.getValue()} />,
+        header: 'Added At',
+      }),
+      columnHelper.accessor((row: RepoTokenDetailDto) => row.appId, {
+        cell: () => (
+          <Button leftIcon={<DeleteIcon />} size="sm" colorScheme="red">
+            Delete
+          </Button>
+        ),
+        header: 'Actions',
+        enableSorting: false,
+      }),
+    ],
+    [columnHelper],
+  );
   return <DynamicDataTable columns={columns as ColumnDef<Data>[]} data={data} />;
 };
 
