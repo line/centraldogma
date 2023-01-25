2192f96ac7907c883381d99f9c8d7532b98f4967
rainy789 <rainyxyz07@gmail.com>
2023-01-25 15:19:00 +0700

    Add more sample data

---
+++ webapp/src/pages/api/v1/projects/[projectName]/repos/[repoName]/commits/[revision].ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { HistoryDto } from 'dogma/features/history/HistoryDto';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faker } from '@faker-js/faker';
+
+export const TOTAL_REVISION = 1000;
+
+const newHistory = (i: number): HistoryDto => {
+  return {
+    revision: i,
+    author: { name: faker.internet.userName(), email: faker.internet.email() },
+    commitMessage: { summary: faker.lorem.sentence(), detail: faker.lorem.paragraph(), markup: 'PLAINTEXT' },
+    pushedAt: faker.datatype.datetime().toISOString(),
+  };
+};
+
+const historyList: HistoryDto[] = [];
+const makeData = (len: number) => {
+  for (let i = len; i > 0; i--) {
+    historyList.push(newHistory(i));
+  }
+};
+
+makeData(TOTAL_REVISION);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { revision, to },
+    method,
+  } = req;
+  const start = -parseInt(revision as string) - 1;
+  switch (method) {
+    case 'GET':
+      res.status(200).json(historyList.slice(start, start + parseInt(to as string)));
+      break;
+    default:
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
+  }
+}
