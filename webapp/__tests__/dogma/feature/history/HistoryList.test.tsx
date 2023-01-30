b994b80e38a7cd078671522cf958caf55b9eb8a9
rainy789 <5079588+rainy789@users.noreply.github.com>
2023-01-30 07:59:55 +0700

    Add controlled pagination for commit history (#793)
    ## Motivation
    Address #56 
    
    ## Modification
    - Add server side pagination based on the following summary. cc @ikhoon 
      - The first low of the first page should start with the latest revision aka. HEAD.
      -  If  a page renders 20 revisions
          - The first page starts with HEAD and ends with HEAD - 20.
          - The second page starts with HEAD - 21 and ends with HEAD - 40
          - If the number of commits returned by server is less then 20, we could assume it is the last page.
    - Add `msw` and `whatwg-fetch` for API endpoint mocking. https://mswjs.io/
    
    ## Result
    - `npm run mock`
    - Navigate to the repository page
    - Click on the history tab
    <img width="1052" alt="Screen Shot 2023-01-25 at 15 24 05" src="https://user-images.githubusercontent.com/5079588/214514011-2a045a05-7340-458a-9114-39685a5cdf14.png">

--- webapp/__tests__/dogma/feature/history/HistoryList.test.tsx
+++ webapp/__tests__/dogma/feature/history/HistoryList.test.tsx
@@ -1,68 +1,91 @@
-import { render, fireEvent } from '@testing-library/react';
-import { HistoryListProps } from 'dogma/features/history/HistoryList';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import { HistoryDto } from 'dogma/features/history/HistoryDto';
 import HistoryList from 'dogma/features/history/HistoryList';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from 'dogma/features/api/apiSlice';
+
+const expectedProps = {
+  projectName: 'ProjectAlpha',
+  repoName: 'RepoGamma',
+  handleTabChange: jest.fn(),
+  totalRevision: 2,
+};
+const mockHistoryList: HistoryDto[] = [
+  {
+    revision: 2,
+    author: { name: 'System', email: 'system@localhost.localdomain' },
+    commitMessage: { summary: 'Update repository', detail: '', markup: 'PLAINTEXT' },
+    pushedAt: '2023-01-11T08:17:22Z',
+  },
+  {
+    revision: 1,
+    author: { name: 'System', email: 'system@localhost.localdomain' },
+    commitMessage: { summary: 'Create a new repository', detail: '', markup: 'PLAINTEXT' },
+    pushedAt: '2023-01-10T08:17:22Z',
+  },
+];
+
+const handlers = [
+  rest.get(
+    `http://localhost/api/v1/projects/${expectedProps.projectName}/repos/${expectedProps.repoName}/commits/-1`,
+    (_, res, ctx) => {
+      return res(ctx.status(200), ctx.json<HistoryDto[]>(mockHistoryList));
+    },
+  ),
+];
 
 describe('HistoryList', () => {
-  let expectedProps: JSX.IntrinsicAttributes & HistoryListProps<object>;
-
-  beforeEach(() => {
-    const mockHistoryList: HistoryDto[] = [
-      {
-        revision: 2,
-        author: { name: 'System', email: 'system@localhost.localdomain' },
-        commitMessage: { summary: 'Update repository', detail: '', markup: 'PLAINTEXT' },
-        pushedAt: '2023-01-11T08:17:22Z',
-      },
-      {
-        revision: 1,
-        author: { name: 'System', email: 'system@localhost.localdomain' },
-        commitMessage: { summary: 'Create a new repository', detail: '', markup: 'PLAINTEXT' },
-        pushedAt: '2023-01-10T08:17:22Z',
-      },
-    ];
-    expectedProps = {
-      data: mockHistoryList,
-      projectName: 'ProjectAlpha',
-      repoName: 'RepoGamma',
-      handleTabChange: jest.fn(),
-    };
+  const server = setupServer(...handlers);
+  beforeAll(() => {
+    server.listen({ onUnhandledRequest: 'error' });
   });
 
-  it('renders a table with a row for each revision', () => {
-    const { container } = render(<HistoryList {...expectedProps} />);
-    expect(container.querySelector('tbody').children.length).toBe(2);
+  afterEach(() => {
+    server.resetHandlers();
   });
 
-  it('links the view icon to `${projectName}/repos/${repoName}/list/${revisionNumber}`', () => {
-    const { container } = render(<HistoryList {...expectedProps} />);
-    const actionCell = container.querySelector('tbody').firstChild.firstChild.lastChild;
-    expect(actionCell).toHaveAttribute(
-      'href',
-      `/app/projects/${expectedProps.projectName}/repos/${expectedProps.repoName}/list/${2}`,
+  afterAll(() => {
+    server.close();
+  });
+
+  it('renders a table with a row for each revision', async () => {
+    const { container } = render(
+      <ApiProvider api={apiSlice}>
+        <HistoryList {...expectedProps} />
+      </ApiProvider>,
     );
+    await waitFor(() => {
+      expect(container.querySelector('tbody').children.length).toBe(2);
+    });
   });
 
-  it('calls handleTabChange when the view icon is clicked', () => {
-    const { container } = render(<HistoryList {...expectedProps} />);
-    const actionCell = container.querySelector('tbody').firstChild.firstChild.lastChild;
-    fireEvent.click(actionCell);
-    expect(expectedProps.handleTabChange).toHaveBeenCalledTimes(1);
-  });
-
-  it('links the revision cell to `${projectName}/repos/${repoName}/list/${revisionNumber}`', () => {
-    const { container } = render(<HistoryList {...expectedProps} />);
-    const firstCell = container.querySelector('tbody').firstChild.firstChild.firstChild;
-    expect(firstCell).toHaveAttribute(
-      'href',
-      `/app/projects/${expectedProps.projectName}/repos/${expectedProps.repoName}/list/${2}`,
+  it('links the revision cell to `${projectName}/repos/${repoName}/list/${revisionNumber}`', async () => {
+    const { container } = render(
+      <ApiProvider api={apiSlice}>
+        <HistoryList {...expectedProps} />
+      </ApiProvider>,
     );
+    await waitFor(() => {
+      const firstCell = container.querySelector('tbody').firstChild.firstChild.firstChild;
+      expect(firstCell).toHaveAttribute(
+        'href',
+        `/app/projects/${expectedProps.projectName}/repos/${expectedProps.repoName}/list/${2}`,
+      );
+    });
   });
 
-  it('calls handleTabChange when the revision cell is clicked', () => {
-    const { container } = render(<HistoryList {...expectedProps} />);
-    const firstCell = container.querySelector('tbody').firstChild.firstChild.firstChild;
-    fireEvent.click(firstCell);
-    expect(expectedProps.handleTabChange).toHaveBeenCalledTimes(1);
+  it('calls handleTabChange when the revision cell is clicked', async () => {
+    const { container } = render(
+      <ApiProvider api={apiSlice}>
+        <HistoryList {...expectedProps} />
+      </ApiProvider>,
+    );
+    await waitFor(() => {
+      const firstCell = container.querySelector('tbody').firstChild.firstChild.firstChild;
+      fireEvent.click(firstCell);
+      expect(expectedProps.handleTabChange).toHaveBeenCalledTimes(1);
+    });
   });
 });
