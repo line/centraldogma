985036a46d04778baa788ce78f679f100d217143
jrhee17 <guins_j@guins.org>
2023-02-09 12:13:00 +0900

    Force revision update when mirroring local to remote (#792)
    **Motivation**
    
    When a mirroring happens, the target commit is checked out and the reference is updated to the commit.
    However, the reference may not be updated if the history is not linear.
    
    Up until now, there wasn't any issue with this since `REMOTE_TO_LOCAL` only required that the commit was checked out correctly. However, `LOCAL_TO_REMOTE` requires that the reference points to the correct commit since it needs to be pushed.
    
    This scenario can occur easily if either
    1) The remote repository is force pushed
    2) A commit is made but it isn't pushed for whatever reason (network failure, auth failure, etc.). The local branch is ahead of the remote branch, so the update reference fails for the next iteration
    
    **Modification**
    - Force update reference when fetching the remote repository
    
    **Result**
    - `LOCAL_TO_REMOTE` functions correctly.

---
+++ it/src/test/java/com/linecorp/centraldogma/it/mirror/git/KeyPairUtilsTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.centraldogma.it.mirror.git;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import org.apache.sshd.common.config.keys.PublicKeyEntry;
+import org.junit.jupiter.api.Test;
+
+import com.jcraft.jsch.JSch;
+
+class KeyPairUtilsTest {
+
+    static String toPemFormat(PrivateKey privateKey) {
+        assert "PKCS#8".equalsIgnoreCase(privateKey.getFormat());
+
+        String key = "-----BEGIN PRIVATE KEY-----";
+        key += "\r\n";
+        key += Base64.getMimeEncoder().encodeToString(privateKey.getEncoded());
+        key += "\r\n";
+        key += "-----END PRIVATE KEY-----";
+        key += "\r\n";
+
+        return key;
+    }
+
+    static String toPemFormat(PublicKey publicKey) {
+        final StringBuilder sb = new StringBuilder();
+        try {
+            PublicKeyEntry.appendPublicKeyEntry(sb, publicKey);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+    @Test
+    void testKeyLoad() throws Exception {
+
+        final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        final KeyPair keyPair = generator.generateKeyPair();
+
+        final String pubKey = toPemFormat(keyPair.getPublic());
+        final String privKey = toPemFormat(keyPair.getPrivate());
+
+        final com.jcraft.jsch.KeyPair loaded = com.jcraft.jsch.KeyPair.load(
+                new JSch(), privKey.getBytes(StandardCharsets.UTF_8), pubKey.getBytes(StandardCharsets.UTF_8));
+        assertThat(loaded.getKeyType()).isEqualTo(com.jcraft.jsch.KeyPair.RSA);
+    }
+}
