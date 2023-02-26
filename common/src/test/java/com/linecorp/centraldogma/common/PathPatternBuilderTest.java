c799e632a446876f5092f993d9aa2b6676d6bd21
Kate Pham <kate.pham@linecorp.com>
2023-02-27 06:31:51 +0700

    remove unused import

--- common/src/test/java/com/linecorp/centraldogma/common/PathPatternBuilderTest.java
+++ common/src/test/java/com/linecorp/centraldogma/common/PathPatternBuilderTest.java
@@ -16,7 +16,6 @@
 package com.linecorp.centraldogma.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
