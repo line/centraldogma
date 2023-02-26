7a06f89784de4814677ccba5a3e18f168fe41998
byugnjun.kim <byungjun.kim@linecorp.com>
2023-02-27 00:09:16 +0900

    Add deletion field

--- server/src/main/java/com/linecorp/centraldogma/server/metadata/Token.java
+++ server/src/main/java/com/linecorp/centraldogma/server/metadata/Token.java
@@ -64,8 +64,11 @@ public final class Token implements Identifiable {
     @Nullable
     private final UserAndTimestamp deactivation;
 
+    @Nullable
+    private final UserAndTimestamp deletion;
+
     Token(String appId, String secret, boolean isAdmin, UserAndTimestamp creation) {
-        this(appId, secret, isAdmin, creation, null);
+        this(appId, secret, isAdmin, creation, null, null);
     }
 
     /**
@@ -76,20 +79,23 @@ public final class Token implements Identifiable {
                  @JsonProperty("secret") String secret,
                  @JsonProperty("admin") boolean isAdmin,
                  @JsonProperty("creation") UserAndTimestamp creation,
-                 @JsonProperty("deactivation") @Nullable UserAndTimestamp deactivation) {
+                 @JsonProperty("deactivation") @Nullable UserAndTimestamp deactivation,
+                 @JsonProperty("deletion") @Nullable UserAndTimestamp deletion) {
         this.appId = Util.validateFileName(appId, "appId");
         this.secret = Util.validateFileName(secret, "secret");
         this.isAdmin = isAdmin;
         this.creation = requireNonNull(creation, "creation");
         this.deactivation = deactivation;
+        this.deletion = deletion;
     }
 
     private Token(String appId, boolean isAdmin, UserAndTimestamp creation,
-                  @Nullable UserAndTimestamp deactivation) {
+                  @Nullable UserAndTimestamp deactivation, @Nullable UserAndTimestamp deletion) {
         this.appId = Util.validateFileName(appId, "appId");
         this.isAdmin = isAdmin;
         this.creation = requireNonNull(creation, "creation");
         this.deactivation = deactivation;
+        this.deletion = deletion;
         secret = null;
     }
 
@@ -141,12 +147,27 @@ public final class Token implements Identifiable {
     }
 
     /**
+     * Returns who delete this token when.
+     */
+    @Nullable
+    @JsonProperty
+    public UserAndTimestamp deletion() {
+        return deletion;
+    }
+
+    /**
      * Returns whether this token is activated.
      */
     @JsonIgnore
     public boolean isActive() {
         return deactivation == null;
     }
+
+    /**
+     * Returns whether this token is deleted.
+     */
+    @JsonIgnore
+    public boolean isDeleted() {return deletion != null;}
 
     @Override
     public String toString() {
@@ -156,6 +177,7 @@ public final class Token implements Identifiable {
                           .add("isAdmin", isAdmin())
                           .add("creation", creation())
                           .add("deactivation", deactivation())
+                          .add("deletion", deletion())
                           .toString();
     }
 
@@ -163,6 +185,6 @@ public final class Token implements Identifiable {
      * Returns a new {@link Token} instance without its secret.
      */
     public Token withoutSecret() {
-        return new Token(appId(), isAdmin(), creation(), deactivation());
+        return new Token(appId(), isAdmin(), creation(), deactivation(), deletion());
     }
 }
