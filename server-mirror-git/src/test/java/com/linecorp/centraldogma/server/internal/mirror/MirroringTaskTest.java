a89d7817b15402270b71a4f5a2150f7b88cae130
minwoox <songmw725@gmail.com>
2023-08-29 11:40:52 +0900

    Fix tests

--- server-mirror-git/src/test/java/com/linecorp/centraldogma/server/internal/mirror/MirroringTaskTest.java
+++ server-mirror-git/src/test/java/com/linecorp/centraldogma/server/internal/mirror/MirroringTaskTest.java
@@ -45,7 +45,7 @@ class MirroringTaskTest {
         Mirror mirror = newMirror("git://a.com/b.git", GitMirror.class, "foo", "bar");
         mirror = spy(mirror);
         doNothing().when(mirror).mirror(any(), any(), anyInt(), anyLong());
-        new MirroringTask(mirror, projectName, meterRegistry).run(null, null, 0, 0L);
+        new MirroringTask(mirror, "foo", meterRegistry).run(null, null, 0, 0L);
         assertThat(MoreMeters.measureAll(meterRegistry))
                 .contains(entry("mirroring.result#count{direction=LOCAL_TO_REMOTE,localPath=/," +
                                 "localRepo=bar,remoteBranch=,remotePath=/,success=true}", 1.0));
@@ -58,7 +58,7 @@ class MirroringTaskTest {
         mirror = spy(mirror);
         final RuntimeException e = new RuntimeException();
         doThrow(e).when(mirror).mirror(any(), any(), anyInt(), anyLong());
-        final MirroringTask task = new MirroringTask(mirror, projectName, meterRegistry);
+        final MirroringTask task = new MirroringTask(mirror, "foo", meterRegistry);
         assertThatThrownBy(() -> task.run(null, null, 0, 0L))
                 .isSameAs(e);
         assertThat(MoreMeters.measureAll(meterRegistry))
@@ -76,7 +76,7 @@ class MirroringTaskTest {
             Thread.sleep(1000);
             return null;
         }).when(mirror).mirror(any(), any(), anyInt(), anyLong());
-        new MirroringTask(mirror, projectName, meterRegistry).run(null, null, 0, 0L);
+        new MirroringTask(mirror, "foo", meterRegistry).run(null, null, 0, 0L);
         assertThat(MoreMeters.measureAll(meterRegistry))
                 .hasEntrySatisfying(
                         "mirroring.task#total{direction=LOCAL_TO_REMOTE,localPath=/," +
