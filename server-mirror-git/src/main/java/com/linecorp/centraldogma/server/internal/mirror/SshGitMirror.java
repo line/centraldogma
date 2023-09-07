443c54623bba3d735f5ea532e2db2e0621bc11d9
minwoox <songmw725@gmail.com>
2023-09-07 15:45:49 +0900

    Fix

--- server-mirror-git/src/main/java/com/linecorp/centraldogma/server/internal/mirror/SshGitMirror.java
+++ server-mirror-git/src/main/java/com/linecorp/centraldogma/server/internal/mirror/SshGitMirror.java
@@ -98,11 +98,11 @@ final class SshGitMirror extends AbstractGitMirror {
         } finally {
             try {
                 if (session != null) {
-                    session.close();
+                    session.close(true);
                 }
             } finally {
                 if (sshClient != null) {
-                    sshClient.close();
+                    sshClient.stop();
                 }
             }
         }
