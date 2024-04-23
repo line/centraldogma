96a680f3de2a867b6dd196e733a9cee5451582c8
Ikhun Um <ih.pert@gmail.com>
2024-04-23 18:06:07 +0900

    grammer

--- testing-internal/src/main/java/com/linecorp/centraldogma/testing/internal/CentralDogmaReplicationExtension.java
+++ testing-internal/src/main/java/com/linecorp/centraldogma/testing/internal/CentralDogmaReplicationExtension.java
@@ -213,7 +213,7 @@ public class CentralDogmaReplicationExtension extends AbstractAllOrEachExtension
             }
         }
 
-        // This logic won't completely prevent port duplication, but it is best efforts to reduce flaky.
+        // This logic won't completely prevent port duplication, but it is best efforts to reduce flakiness.
         boolean success = true;
         for (int i = 0; i < MAX_RETRIES; i++) {
             success = true;
