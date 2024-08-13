b06bc1129ff3d936bc3cc0d2244a29bcfd631926
ByungJun <byungjun.kim@linecorp.com>
2024-08-13 20:40:39 +0900

    Add HTTP API for updating a token level (#942)
    Motivation:
    - Resolve https://github.com/line/centraldogma/issues/906
    - A lot of effort is used to change permissions.
    
    Modification:
    - Change admin from true to false or vice versa.
    
    Result:
    - You can now change the level of a token via HTTP API.
    <img width="805" alt="image" src="https://github.com/line/centraldogma/assets/19267206/9c1a527e-9bb9-4995-bf36-b284e4c644a8">
    <img width="796" alt="image" src="https://github.com/line/centraldogma/assets/19267206/1709909d-6b4e-48f6-b4b6-0c078388508d">

--- server/src/test/java/com/linecorp/centraldogma/server/internal/api/TokenServiceTest.java
+++ server/src/test/java/com/linecorp/centraldogma/server/internal/api/TokenServiceTest.java
@@ -15,9 +15,11 @@
  */
 package com.linecorp.centraldogma.server.internal.api;
 
+import static com.linecorp.centraldogma.internal.api.v1.HttpApiV1Constants.API_V1_PATH_PREFIX;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.concurrent.CompletionException;
 
@@ -26,17 +28,30 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpData;
+import com.linecorp.armeria.common.HttpHeaderNames;
 import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.QueryParams;
+import com.linecorp.armeria.common.RequestHeaders;
+import com.linecorp.armeria.common.auth.AuthToken;
 import com.linecorp.armeria.server.HttpResponseException;
 import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.centraldogma.common.Author;
 import com.linecorp.centraldogma.common.ProjectRole;
 import com.linecorp.centraldogma.internal.Jackson;
+import com.linecorp.centraldogma.internal.api.v1.AccessToken;
+import com.linecorp.centraldogma.server.CentralDogmaBuilder;
 import com.linecorp.centraldogma.server.command.Command;
 import com.linecorp.centraldogma.server.command.StandaloneCommandExecutor;
 import com.linecorp.centraldogma.server.metadata.MetadataService;
@@ -44,6 +59,9 @@ import com.linecorp.centraldogma.server.metadata.Token;
 import com.linecorp.centraldogma.server.metadata.Tokens;
 import com.linecorp.centraldogma.server.metadata.User;
 import com.linecorp.centraldogma.testing.internal.ProjectManagerExtension;
+import com.linecorp.centraldogma.testing.internal.auth.TestAuthMessageUtil;
+import com.linecorp.centraldogma.testing.internal.auth.TestAuthProviderFactory;
+import com.linecorp.centraldogma.testing.junit.CentralDogmaExtension;
 
 import io.netty.util.internal.StringUtil;
 
@@ -51,6 +69,16 @@ class TokenServiceTest {
 
     @RegisterExtension
     static final ProjectManagerExtension manager = new ProjectManagerExtension();
+
+    @RegisterExtension
+    static final CentralDogmaExtension dogma = new CentralDogmaExtension() {
+
+        @Override
+        protected void configure(CentralDogmaBuilder builder) {
+            builder.administrators(TestAuthMessageUtil.USERNAME);
+            builder.authProviderFactory(new TestAuthProviderFactory());
+        }
+    };
 
     private static final Author adminAuthor = Author.ofEmail("admin@localhost.com");
     private static final Author guestAuthor = Author.ofEmail("guest@localhost.com");
@@ -69,13 +97,26 @@ class TokenServiceTest {
 
     private static TokenService tokenService;
     private static MetadataService metadataService;
+    private static WebClient adminClient;
 
     // ctx is only used for getting the blocking task executor.
     private final ServiceRequestContext ctx =
             ServiceRequestContext.builder(HttpRequest.of(HttpMethod.GET, "/")).build();
 
+    static String sessionId(WebClient webClient, String username, String password)
+            throws JsonParseException, JsonMappingException {
+        return Jackson.readValue(TestAuthMessageUtil.login(webClient, username, password).content().array(),
+                                 AccessToken.class).accessToken();
+    }
+
     @BeforeAll
-    static void setUp() {
+    static void setUp() throws JsonMappingException, JsonParseException {
+        final URI uri = dogma.httpClient().uri();
+        adminClient = WebClient.builder(uri)
+                               .auth(AuthToken.ofOAuth2(sessionId(dogma.httpClient(),
+                                                                  TestAuthMessageUtil.USERNAME,
+                                                                  TestAuthMessageUtil.PASSWORD)))
+                               .build();
         metadataService = new MetadataService(manager.projectManager(), manager.executor());
         tokenService = new TokenService(manager.executor(), metadataService);
     }
@@ -202,5 +243,52 @@ class TokenServiceTest {
                 () -> tokenService.updateToken(ctx, "forUpdate", activation, adminAuthor, admin).join())
                 .isInstanceOf(CompletionException.class)
                 .hasCauseInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void updateTokenLevel() {
+        final Token token = tokenService.createToken("forUpdate", false, null, adminAuthor, admin).join()
+                                        .content();
+        assertThat(token.isActive()).isTrue();
+
+        final Token userToken = tokenService.updateTokenLevel(ctx, "forUpdate", new TokenLevelRequest("ADMIN"),
+                                                              adminAuthor, admin)
+                                            .join();
+        assertThat(userToken.isAdmin()).isTrue();
+
+        final Token adminToken = tokenService.updateTokenLevel(ctx, "forUpdate", new TokenLevelRequest("USER"),
+                                                               adminAuthor, admin)
+                                             .join();
+        assertThat(adminToken.isAdmin()).isFalse();
+
+        assertThatThrownBy(
+                () -> tokenService.updateTokenLevel(ctx, "forUpdate", new TokenLevelRequest("INVALID"),
+                                                    adminAuthor, admin).join())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void createTokenAndUpdateLevel() throws JsonParseException {
+        assertThat(adminClient.post(API_V1_PATH_PREFIX + "tokens",
+                                    QueryParams.of("appId", "forUpdate", "isAdmin", false),
+                                    HttpData.empty())
+                              .aggregate()
+                              .join()
+                              .headers()
+                              .get(HttpHeaderNames.LOCATION)).isEqualTo("/tokens/forUpdate");
+
+        final RequestHeaders headers = RequestHeaders.of(HttpMethod.PATCH,
+                                                         API_V1_PATH_PREFIX + "tokens/forUpdate/level",
+                                                         HttpHeaderNames.CONTENT_TYPE, MediaType.JSON);
+
+        final String body = "{\"level\":\"ADMIN\"}";
+        final AggregatedHttpResponse response = adminClient.execute(headers, body).aggregate().join();
+
+        final JsonNode jsonNode = Jackson.readTree(response.contentUtf8());
+        assertThat(jsonNode.get("appId").asText()).isEqualTo("forUpdate");
+        assertThat(jsonNode.get("admin").asBoolean()).isEqualTo(true);
+
+        final AggregatedHttpResponse response2 = adminClient.execute(headers, body).aggregate().join();
+        assertThat(response2.status()).isEqualTo(HttpStatus.NOT_MODIFIED);
     }
 }
