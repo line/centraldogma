cb0e77cffdc7f579efa4456bd43c2325644058b2
minwoox <songmw725@gmail.com>
2024-08-13 18:58:34 +0900

    Check if the internal project and repos exist before creating them Motivation: Central Dogma server currently attempts to create internal projects and repositories during starting up, even if they already exist. This is unnecessary for most scenarios, except when setting up a new cluster.
    Modifications:
    - Added checks to verify if the internal project and repositories exist before attempting to create them.
    - Propagated the read-only exception to the caller if it's raised while creating the internal project.
      - It doesn't make sense to continue to run the cluster because the internal project must exist when the cluster gets back to non read-only mode.
    
    Result:
    - Central Dogma servers now checks for the existence of internal projects and repos before attempting to create them.

--- server/src/main/java/com/linecorp/centraldogma/server/storage/project/InternalProjectInitializer.java
+++ server/src/main/java/com/linecorp/centraldogma/server/storage/project/InternalProjectInitializer.java
@@ -25,14 +25,17 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableList;
 
 import com.linecorp.armeria.common.util.Exceptions;
 import com.linecorp.centraldogma.common.Author;
 import com.linecorp.centraldogma.common.Change;
 import com.linecorp.centraldogma.common.ChangeConflictException;
+import com.linecorp.centraldogma.common.Entry;
 import com.linecorp.centraldogma.common.Markup;
 import com.linecorp.centraldogma.common.ProjectExistsException;
+import com.linecorp.centraldogma.common.Query;
 import com.linecorp.centraldogma.common.ReadOnlyException;
 import com.linecorp.centraldogma.common.RepositoryExistsException;
 import com.linecorp.centraldogma.common.Revision;
@@ -49,13 +52,15 @@ public final class InternalProjectInitializer {
     public static final String INTERNAL_PROJECT_DOGMA = "dogma";
 
     private final CommandExecutor executor;
+    private final ProjectManager projectManager;
     private final CompletableFuture<Void> initialFuture = new CompletableFuture<>();
 
     /**
      * Creates a new instance.
      */
-    public InternalProjectInitializer(CommandExecutor executor) {
+    public InternalProjectInitializer(CommandExecutor executor, ProjectManager projectManager) {
         this.executor = executor;
+        this.projectManager = projectManager;
     }
 
     /**
@@ -86,17 +91,15 @@ public final class InternalProjectInitializer {
      */
     public void initialize0(String projectName) {
         final long creationTimeMillis = System.currentTimeMillis();
-        try {
-            executor.execute(createProject(creationTimeMillis, Author.SYSTEM, projectName))
-                    .get();
-        } catch (Throwable cause) {
-            final Throwable peeled = Exceptions.peel(cause);
-            if (peeled instanceof ReadOnlyException) {
-                // The executor has stopped right after starting up.
-                return;
-            }
-            if (!(peeled instanceof ProjectExistsException)) {
-                throw new Error("failed to initialize an internal project: " + projectName, peeled);
+        if (!projectManager.exists(projectName)) {
+            try {
+                executor.execute(createProject(creationTimeMillis, Author.SYSTEM, projectName))
+                        .get();
+            } catch (Throwable cause) {
+                final Throwable peeled = Exceptions.peel(cause);
+                if (!(peeled instanceof ProjectExistsException)) {
+                    throw new Error("failed to initialize an internal project: " + projectName, peeled);
+                }
             }
         }
 
@@ -106,6 +109,13 @@ public final class InternalProjectInitializer {
     }
 
     private void initializeTokens() {
+        final Entry<JsonNode> entry =
+                projectManager.get(INTERNAL_PROJECT_DOGMA).repos().get(Project.REPO_DOGMA)
+                              .getOrNull(Revision.HEAD,
+                                         Query.ofJson(MetadataService.TOKEN_JSON)).join();
+        if (entry != null && entry.hasContent()) {
+            return;
+        }
         try {
             final Change<?> change = Change.ofJsonPatch(MetadataService.TOKEN_JSON,
                                                         null, Jackson.valueToTree(new Tokens()));
@@ -116,7 +126,7 @@ public final class InternalProjectInitializer {
                     .get();
         } catch (Throwable cause) {
             final Throwable peeled = Exceptions.peel(cause);
-            if (peeled instanceof ReadOnlyException || peeled instanceof ChangeConflictException) {
+            if (peeled instanceof ChangeConflictException) {
                 return;
             }
             throw new Error("failed to initialize the token list file", peeled);
@@ -137,7 +147,12 @@ public final class InternalProjectInitializer {
     private void initializeInternalRepos(String projectName, List<String> internalRepos,
                                          long creationTimeMillis) {
         requireNonNull(internalRepos, "internalRepos");
+        final Project project = projectManager.get(projectName);
+        assert project != null;
         for (final String repo : internalRepos) {
+            if (project.repos().exists(repo)) {
+                continue;
+            }
             try {
                 executor.execute(createRepository(creationTimeMillis, Author.SYSTEM, projectName, repo))
                         .get();
